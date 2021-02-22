5f1827b70d18f3cbe7cb94c1140b96ae6ed16637
hyn <mrsay132@gmail.com>
2021-02-22 17:27:33 +0700

    tổng chẵn bình phương

---
+++ N0304B-tongchanbinhphuong.cpp
@@ -0,0 +1,11 @@
+//http://laptrinhphothong.vn/Problem/Details/5806
+#include <iostream>
+using namespace std;
+int main(){
+    long long n;
+    cin>>n;
+    long long s = ( 4 * n * ( n + 1 ) * ( 2 * n + 1 ))/6;
+    cout<<s;
+    return 0;
+}
+    
