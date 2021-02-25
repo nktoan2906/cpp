54457c48b04f56f95da0f736d29237082a2c078d
hyn <mrsay132@gmail.com>
2021-02-25 19:57:11 +0700

    tổng lẻ

---
+++ N0109B-tongle.cpp
@@ -0,0 +1,12 @@
+//http://laptrinhphothong.vn/Problem/Details/5617
+#include <iostream>
+using namespace std;
+long long n;
+int main()
+{
+    cin>>n;
+    long long s = (n+1)*(n+1);
+    cout<<s;
+    return 0;
+}
+    
