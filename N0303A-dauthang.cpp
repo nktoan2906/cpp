dda7268f90e694687678085becf25c466101b715
hyn <mrsay132@gmail.com>
2021-02-25 19:44:37 +0700

    dấu thăng

---
+++ N0303A-dauthang.cpp
@@ -0,0 +1,19 @@
+//http://laptrinhphothong.vn/Problem/Details/4573
+#include <bits/stdc++.h>
+using namespace std;
+long long n;
+int main()
+{
+    cin>>n;
+    for (int i=1; i<=3; i++)
+    {
+        for (int j=1; j<=n; j++)
+        {
+            cout<<"#";
+        }
+        n--;
+        cout<<endl;
+    }
+    return 0;
+}
+    
