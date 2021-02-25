500defe936d577d873ee42da264d6c2e236288c1
hyn <mrsay132@gmail.com>
2021-02-25 19:41:35 +0700

    bảng ký tự

---
+++ N0311B-bangkytu.cpp
@@ -0,0 +1,17 @@
+//http://laptrinhphothong.vn/Problem/Details/4581
+#include <bits/stdc++.h>
+using namespace std;
+long long n,m;
+char q;
+int main()
+{
+    cin>>n>>m>>q;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            cout<<q;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+    
