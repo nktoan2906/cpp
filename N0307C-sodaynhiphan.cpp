bace9d9ed9aff853573ffd92f6580f5ed0368eb9
hyn <mrsay132@gmail.com>
2021-02-25 19:36:07 +0700

    số dãy nhị phân

---
+++ N0307C-sodaynhiphan.cpp
@@ -0,0 +1,13 @@
+//http://laptrinhphothong.vn/Problem/Details/4577
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    long long n;
+    cin>>n;
+    long long res = pow(2,n);
+    cout<< res;
+    return 0;
+}
+    
