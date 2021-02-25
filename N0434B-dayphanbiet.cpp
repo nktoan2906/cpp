d9bedcdbba4de2e521e770336ee75a2e0904ccc8
hyn <mrsay132@gmail.com>
2021-02-25 20:22:27 +0700

    dãy phân biệt

---
+++ N0434B-dayphanbiet.cpp
@@ -0,0 +1,30 @@
+//http://laptrinhphothong.vn/Problem/Details/5776
+#include <bits/stdc++.h>
+using namespace std;
+long long j, n, dem = 0;
+int a[100];
+main()
+{
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n - 1; i++)
+    {
+        j = i + 1;
+        while (j < n)
+            if (a[i] == a[j])
+            {
+                for (int k = j; k < n - 1; k++)
+                    a[k] = a[k + 1];
+                n = n - 1;
+            }
+            else
+                j = j + 1;
+    }
+    n--;
+    for (int i = 0; i < n + 1; i++)
+    {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
