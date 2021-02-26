61b218525cef213bc5e9bf0d512e69b9db4ce159
hyn <mrsay132@gmail.com>
2021-02-26 14:12:08 +0700

    số lớn hơn #1

---
+++ N0204A-solonhon1.cpp
@@ -0,0 +1,12 @@
+//http://laptrinhphothong.vn/Problem/Details/5598
+#include <iostream>
+using namespace std;
+long long a,b;
+int main()
+{
+    cin >> a >> b;
+    if(a>b) cout<<a;
+    else cout<<b;
+    return 0;
+}
+    
