43f26df05ba7aae2c1d0b0d1fc7a62827c56d82a
hyn <mrsay132@gmail.com>
2021-02-26 14:23:16 +0700

    in ra các i từ b tới a

---
+++ PK0011A-BA.cpp
@@ -0,0 +1,11 @@
+//http://laptrinhphothong.vn/Problem/Details/5872
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long a,b;
+    cin>>a>>b;
+    for(int i=b;i>=a;i--) cout<<i<<" ";
+    return 0;
+}
+    
