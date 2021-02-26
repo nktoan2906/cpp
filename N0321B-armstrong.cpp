b989a0a5d557385ad95339e15505ef9defb3af85
hyn <mrsay132@gmail.com>
2021-02-26 13:59:19 +0700

    check số armstrong

---
+++ N0321B-armstrong.cpp
@@ -0,0 +1,31 @@
+//http://laptrinhphothong.vn/Problem/Details/5764
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int n;
+    while(cin >> n)
+    {
+        int S = 0;
+        int SoChuSo = 0;
+        int p = n;
+        int q = n;
+        while(n>0)
+        {
+            n=n/10;
+            SoChuSo += 1;
+        }
+        for(int l = 1; l <= SoChuSo ; l++)
+        {
+            int m = p % 10;
+            p = p / 10;
+            S += pow(m, SoChuSo);
+        }
+        if (S == q) cout << "YES" ;
+        else cout << "NO" ;
+    }
+    return 0;
+}
+    
