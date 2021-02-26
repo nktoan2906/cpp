869b468eb20922aa9ab3f0dae28531689d7befc2
hyn <mrsay132@gmail.com>
2021-02-26 14:06:30 +0700

    dấu đô la

---
+++ N0304A-daudollar.cpp
@@ -0,0 +1,17 @@
+//http://laptrinhphothong.vn/Problem/Details/4574
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, i, j;
+    cin >> a;
+    for (i = 1; i <= a; i++)
+    {
+        for (j = 1; j <= a; j++)
+        {
+            cout << "$"; //luu y,co mot khoang trong o day
+        }
+        cout << "\n";
+    }
+    return 0;
+}
