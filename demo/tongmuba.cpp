890d78f1e6ec9c7a92413aea67ec99bd5eaad113
hyn <mrsay132@gmail.com>
2021-03-04 21:41:53 +0700

    n^3

---
+++ demo/tongmuba.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long n, tong = 0;
+    long long mod = 1e9 + 7;
+
+    cin >> n;
+    for(long long i(1); i <= n; i++)
+        tong = (tong + i * i * i) % mod;
+    cout << tong;
+    return 0;
+}
