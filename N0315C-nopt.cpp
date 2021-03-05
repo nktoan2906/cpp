ede1535058abf0b090ecb643f2758538970a05b4
hyn <mrsay132@gmail.com>
2021-03-05 20:42:35 +0700

    tính tổng không bt số pt

---
+++ N0315C-nopt.cpp
@@ -0,0 +1,13 @@
+//http://laptrinhphothong.vn/Problem/Details/5640
+#include <iostream>
+using namespace std;
+long long s;
+int main()
+{
+    long long n;
+    while (cin >> n){
+        s += n;
+    }
+    cout << s;
+    return 0;
+}
