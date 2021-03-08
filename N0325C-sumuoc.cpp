8edbf663d4eeaa43746291974c22d98696034102
hyn <mrsay132@gmail.com>
2021-03-08 12:17:03 +0700

    tổng ước

---
+++ N0325C-sumuoc.cpp
@@ -0,0 +1,22 @@
+//http://laptrinhphothong.vn/Problem/Details/5770
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	unsigned long long n;
+	unsigned long long s = 0;
+	cin >> n;
+	for (unsigned long long i = 1; i <= sqrt(n); i++)
+	{
+		if (n % i == 0)
+		{
+			unsigned long long j = n / i;
+			if (i == j) s = s + i;
+			else s = s + i + j; 
+		}
+	}
+	cout << (s);
+	return 0;
+}
