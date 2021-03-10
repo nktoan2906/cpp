23c73e3459f387d244b3f48bcdc1e306641e1ec1
hyn <mrsay132@gmail.com>
2021-03-10 22:28:44 +0700

    tìm pt lẻ tối thiểu trong mảng

---
+++ N0405A-oddmin.cpp
@@ -0,0 +1,24 @@
+//http://laptrinhphothong.vn/Problem/Details/4594
+#include <iostream>
+using namespace std;
+int main()
+{
+	long long n, a, min, max;
+	cin >> n;
+	min = -1;
+	bool minInit = false;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a;
+		if (a % 2 != 0)
+		{
+			if (!minInit || a < min)
+			{
+				minInit = true;
+				min = a;
+			}
+		}
+	}
+	cout << min;
+	return 0;
+}
