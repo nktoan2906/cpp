f7bc27df3293bf4ebecac1379e8c32cd376e6ae0
hyn <mrsay132@gmail.com>
2021-03-14 16:41:41 +0700

    tính tổ hợp chập k của n phần tử

---
+++ IMA2004A-tohop.cpp
@@ -0,0 +1,14 @@
+//http://laptrinhphothong.vn/Problem/Details/5656
+#include<iostream>
+using namespace std;
+int C(int k, int n) {
+    if (k == 0 || k == n) return 1;
+    if (k == 1) return n;
+    return C(k - 1, n - 1) + C(k, n - 1);
+}
+int main(){
+    int n, k;
+    cin>>k>>n;
+    cout << C(k, n);
+    return 0;
+}
