4d5184f199fd1f6c862a42e01a4f61c364310696
hyn <mrsay132@gmail.com>
2021-03-18 22:19:55 +0700

    tính tổng giai thừa

---
+++ demo/N0310C-sumgiaithua.cpp
@@ -0,0 +1,16 @@
+//http://laptrinhphothong.vn/Problem/Details/4580
+#include <iostream>
+using namespace std;
+unsigned long long n,s,f;
+int main()
+{
+	cin >> n;
+    f = 1;
+	s = 0;
+	long long mod = 1e9 + 7;
+	for(long long i = 1;i<=n;i++){
+		f = (f*i) % mod;
+		s = (s+f) % mod;
+	}
+	cout<<s;
+}
