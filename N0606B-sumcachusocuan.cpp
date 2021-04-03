cec2ce57667b85dbd1876456e167dffa21ceeb57
hyn <mrsay132@gmail.com>
2021-04-03 10:32:48 +0700

    in ra tổng các chữ số của n

---
+++ N0606B-sumcachusocuan.cpp
@@ -0,0 +1,14 @@
+//http://laptrinhphothong.vn/Problem/Details/4626
+#include<iostream>
+using namespace std;
+int main(){
+	int sum = 0;
+	string s;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++)
+	{
+		int x = s[i] - 48;
+		sum += x;
+	}
+	cout << sum; 
+}
