917829adc74375b7acb305aebfbeb04883dcbedb
hyn <mrsay132@gmail.com>
2021-04-07 09:24:57 +0700

    k-i*n until m = n thì thôi

---
+++ N0327B-echnhay.cpp
@@ -0,0 +1,15 @@
+//http://laptrinhphothong.vn/Problem/Details/5801
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    while (k % n != 0) {
+        int m = k % n;
+        k = m + n;
+        n = m;
+    }
+    cout << n;
+}
+    
