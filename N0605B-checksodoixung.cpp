564bcc4c5d2d808a071b60dab1a84c47a6a6e33f
hyn <mrsay132@gmail.com>
2021-04-08 22:45:14 +0700

    check số đối xứng

---
+++ N0605B-checksodoixung.cpp
@@ -0,0 +1,13 @@
+//http://laptrinhphothong.vn/Problem/Details/4625
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+    string s, s1="";
+    getline(cin,s);
+    for (int i=s.length()-1; i>=0; i--) s1+=s[i];
+    if (s1==s) cout << "YES";
+    else cout << "NO";
+    return 0;
+}
