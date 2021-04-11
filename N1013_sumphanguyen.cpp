6d5637e406a4c29a00dc305de38230eda3ce9748
hyn <mrsay132@gmail.com>
2021-04-11 22:34:56 +0700

    S=[n/1] +[n/2] +...+[n/n]

---
+++ N1013_sumphanguyen.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#define ll long long
+using namespace std;
+ll int sum(ll int n)
+{
+    ll int root = sqrt(n);
+    ll int s = 0; 
+    for (int i = 1; i <= root; i++) {
+        s += n / i;
+    }
+    s = 2 * s - (root * root);
+    return s;
+}
+int main()
+{
+    ll int n ;
+    cin>>n;
+    cout << sum(n);
+    return 0;
+}
