438b9cbe0d3201040b669d72741b1d6639bc8155
hyn <mrsay132@gmail.com>
2021-04-18 20:18:13 +0700

    tìm snt max ko vượt quá n

---
+++ demo/N0807B-maxsnt.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+bool is_prime(int n)
+{
+    bool kt = true;
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            kt = false;
+            break;
+        }
+    }
+    return kt;
+}
+int main()
+{
+    int n,x = 0;
+    cin >> n;
+    n--;
+    while (x == 0) {
+        if (is_prime(n)) x = n;
+        else n--;
+    }
+    cout << x;
+}
