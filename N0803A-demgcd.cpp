bc3d3f3ae9cb799908c08b568e813fe0ec424427
hyn <mrsay132@gmail.com>
2021-04-24 06:30:14 +0700

    đếm num cặp TM gcd(ai,aj) > 1

---
+++ N0803A-demgcd.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ll n,a[101],dem = 0;
+    cin>>n;
+    for(int i=1;i<=n;i++) cin>>a[i];
+    for(int i=1;i<=n-1;i++){
+        for(int j=i+1;j<=n;j++){
+            if(__gcd(a[i],a[j]) > 1) dem++;
+        }
+    }
+    cout<<dem;
+    return 0;
+}
