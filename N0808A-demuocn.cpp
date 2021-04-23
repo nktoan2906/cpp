56f97a1896026ded5130a3a3080539f054bb0de0
hyn <mrsay132@gmail.com>
2021-04-23 18:00:37 +0700

    đếm ước của n

--- N0808A-demuocn.cpp
+++ N0808A-demuocn.cpp
@@ -1,20 +1,19 @@
 //http://laptrinhphothong.vn/Problem/Details/5823
-#include<iostream>
-#include<math.h>
+#include <iostream>
+#include <math.h>
+#define ll long long
 using namespace std;
 int main()
 {
-      long long n,i=1,d=0;
-      cin>>n;
-      while(i<=sqrt(n))
-         {
-          if(n%i==0)
-           {
-            d++;
-             if(i!=(n/i)) d++;
-           }
-          i++; 
-         }
-   cout<<d;
-   return 0;
+      ll n, i = 1, d = 0;
+      cin >> n;
+      while (i <= sqrt(n)) {
+            if (n % i == 0) {
+                  d++;
+                  if (i != (n / i)) d++;
+            }
+            i++;
+      }
+      cout << d;
+      return 0;
 }
