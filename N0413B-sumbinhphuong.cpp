9d42ed7fc5917b568db851dd414ed76c5f2b4744
hyn <mrsay132@gmail.com>
2021-04-28 20:56:21 +0700

    in ra max của ai^2 + aj^2

---
+++ N0413B-sumbinhphuong.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+ll max1 = 0, max2 = 0;
+int main()
+{
+    ll n; cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        ll a;
+        cin >> a; //  nhập mảng 
+        a = abs(a); 
+        if (a > max1) {
+            max1 = a; // nếu max1 < a thì gán bằng 1 
+            max2 = max1; // 
+        }
+        else max2 = max(max2, a); 
+    }
+    cout << max1 * max1 + max2 * max2;
+}
