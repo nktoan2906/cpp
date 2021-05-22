58fc28df7d514ec7673f42dd38f988d276e7bbed
hyn <mrsay132@gmail.com>
2021-05-22 07:06:49 +0700

    xoá các chữ số trong chuỗi

---
+++ N0608B-xoachuso.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+#include <string> 
+#include <regex> 
+using namespace std; 
+int main (void) 
+{ 
+   string MyString; 
+   getline(cin, MyString); 
+   regex digit("[[:digit:]]"); 
+   cout<< regex_replace(MyString, digit, "") << endl; 
+   return 0; 
+} 
