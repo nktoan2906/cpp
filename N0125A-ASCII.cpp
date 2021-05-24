e93ea113121fe5106c04f144a196b1e2c7f44d7c
hyn <mrsay132@gmail.com>
2021-05-25 06:57:07 +0700

    in ra mã ASCII của kí tự đó

--- N0125A-ASCII.cpp
+++ N0125A-ASCII.cpp
@@ -1,11 +1,9 @@
-//http://laptrinhphothong.vn/Problem/Details/5953
 #include <iostream>
 using namespace std;
-
-int main()
-{
+int main() {
 	char c;
 	cin >> c;
 	cout << int(c);
 	return 0;
 }
+    
