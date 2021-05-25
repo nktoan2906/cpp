965c898ad5ae7a878f302a13c7f14a2b6b247802
hyn <mrsay132@gmail.com>
2021-05-25 07:25:56 +0700

    check trong mảng CÓ pt chia hết cho 7 hay ko

---
+++ N0425A-checkptdivide7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+bool checkdivide7(int a[], int n){
+    bool check;
+    for(int i=1;i<=n;i++){
+        if(a[i] % 7 == 0) check = true;
+        else check = false;
+    }
+    return check;
+}
+int main()
+{
+    int n,a[101];
+    cin>>n;
+    for(int i=1;i<=n;i++) cin>>a[i];
+    if(checkdivide7(a,n) == true) cout<<"YES";
+    else cout<<"NO";
+    
+}
