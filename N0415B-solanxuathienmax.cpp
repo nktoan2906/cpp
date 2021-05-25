ab834f89985c0a1c90a984824a499c0e3d03b78e
hyn <mrsay132@gmail.com>
2021-05-25 12:46:47 +0700

    in numax của pt xuất hiện max trong mảng

--- N0415B-solanxuathienmax.cpp
+++ N0415B-solanxuathienmax.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 using namespace std;
-int a[100];
+int a[101];
 int main()
 {
-    int i, n, j, max, gt, dem;
+    int n;
     cin>>n;
-    for (i = 0; i < n; i++) cin>>a[i];
-    max = 1;
-    for (i = 0; i < n; i++) {
-        dem = 0;
-        for (j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++) cin>>a[i];
+    int max = 1;
+    for (int i = 0; i < n; i++) {
+        int dem = 0;
+        for (int j = 0; j < n; j++) {
             if (a[i] == a[j]) dem++;
         }
-        if (max < dem) {
-            max = dem;
-            gt = a[i];
-        }
+        if (max < dem) max = dem; 
     }
     cout<<max;
 }
