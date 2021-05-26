f99b7492a01c7561cf951500c4eb19a3cd1c8404
hyn <mrsay132@gmail.com>
2021-05-26 20:55:42 +0700

    check số cặp

--- N0202B-socap.cpp
+++ N0202B-socap.cpp
@@ -1,12 +1,12 @@
 //http://laptrinhphothong.vn/Problem/Details/5888
 #include <iostream>
 using namespace std;
-bool kiemtra(int n) {
+bool Check(int n) {
     return n % 11 == 0 && n < 100 && n >= 10;
 }
 int main() {
     long long n;
     cin >> n;
-    if (kiemtra(n)) cout << "YES";
+    if (Check(n)) cout << "YES";
     else cout << "NO";
 }
