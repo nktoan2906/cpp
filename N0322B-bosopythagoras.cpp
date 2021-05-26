df344b765b5743b50b73d82ace975a1d3fb475a8
hyn <mrsay132@gmail.com>
2021-05-26 21:50:50 +0700

    in ra bộ số Pythagoras mà cả 3 đều <=n

--- N0322B-bosopythagoras.cpp
+++ N0322B-bosopythagoras.cpp
@@ -2,19 +2,15 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-int main()
-{
+int main() {
     long long n,x,y,i,j,k,l,tong;
 	cin>>n;
 	long long a=1;
 	long long b=0;
 	if (n<5) cout<<-1;
-	else
-	{
-	    for (i=1;i<n;i++)
-        {
-	        for (j=i+1;j<n;j++)
-	        {
+	else {
+	    for (i=1;i<n;i++) {
+	        for (j=i+1;j<n;j++) {
                 double z=sqrt(i*i+j*j);
                 if((int)z==z && ( z<n || z==n)) cout<<i<<" "<<j<<" "<<(int)z<<endl;
 	        }
