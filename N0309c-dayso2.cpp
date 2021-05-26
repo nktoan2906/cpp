2da0505ac98bc082e8e46e95f3a3bf5540bcc3d5
hyn <mrsay132@gmail.com>
2021-05-26 21:15:29 +0700

    in ra sô hạng thứ n của dãy

--- N0309c-dayso2.cpp
+++ N0309c-dayso2.cpp
@@ -3,12 +3,10 @@
 using namespace std;
 const long long mod = 1e9 + 7;
 long long a[1000002],n;
-int main()
-{
+int main() {
     cin>>n;
     a[1] = a[2] = 1;
-    for( long long i=3; i<=n; ++i)
-    {
+    for( long long i=3; i<=n; ++i) {
         a[i] = ((3*a[i-1] % mod ) - (a[i-2] % mod)) % mod;
         if (a[i] < 0) a[i] = 1e9 + 7 - a[i] * -1;
     }
