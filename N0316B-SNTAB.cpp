346b02685471bf52afb5222e1027e618f72e2c2e
hyn <mrsay132@gmail.com>
2021-05-26 21:35:42 +0700

    in ra các SNT trong đoạn [a,b]

---
+++ N0316B-SNTAB.cpp
@@ -0,0 +1,22 @@
+//http://laptrinhphothong.vn/Problem/Details/5762
+#include <iostream>
+using namespace std;
+bool CheckSNT(long long n) {
+	for (int i = 2; i * i <= n; i++) {
+		if (n % i == 0) return false;
+	}
+	return n > 1;
+}
+int dem = 0;
+int main() {
+	int i, a, b;
+	cin >> a >> b;
+	for (i = a; i <= b; i++) {
+		if (CheckSNT(i)) {
+			cout << i;
+			dem++;
+		}
+	}
+	if (dem == 0) cout << -1;
+	return 0;
+}
