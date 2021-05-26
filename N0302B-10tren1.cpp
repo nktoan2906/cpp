159bdba3180740fb9601332fd76e081f064406c3
hyn <mrsay132@gmail.com>
2021-05-26 21:06:24 +0700

    in ra 10 số trên 1 dòng

--- N0302B-10tren1.cpp
+++ N0302B-10tren1.cpp
@@ -1,9 +1,8 @@
 //http://laptrinhphothong.vn/Problem/Details/5802
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 long long n;
-int main()
-{
+int main() {
     cin>>n;
     for(int i=1;i<=n;i++){
         if(i%10==0) cout<<i<<endl;
