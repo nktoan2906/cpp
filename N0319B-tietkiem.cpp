1b2dc8e5c91fac2d73a5f09621e09b0407a087cb
hyn <mrsay132@gmail.com>
2021-05-26 21:46:00 +0700

    in ra số tiền tiết kiệm có đc sau 1 ngày

--- N0319B-tietkiem.cpp
+++ N0319B-tietkiem.cpp
@@ -2,15 +2,14 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-int main()
-{
+int main() {
     long long n;
     cin>>n;
-    long long ngaydau = 2000;
+    long long n0 = 2000;
     long long kq = 0;
     for(int i=1;i<=n;i++){
-        kq += ngaydau;
-        ngaydau += 1000; 
+        kq += n0;
+        n0 += 1000; 
     }
     cout<<kq;
     return 0;
