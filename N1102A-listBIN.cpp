dfca40e823e3b97a0c33ab1d61ba9e41b59d3031
hyn <mrsay132@gmail.com>
2021-05-26 22:07:30 +0700

    liệt kê các dãy nhị phân độ dài n

--- N1102A-listBIN.cpp
+++ N1102A-listBIN.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
-int main()
-{
+int main() {
 	int n;
 	cin >> n; // yeu cau nguoi dung nhap vao do dai chuoi n
 	// khoi tao mang n phan tu va gan cac phan tu cua mang bang 0
