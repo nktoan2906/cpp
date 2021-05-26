1e60d656c4464fdd1c1c0819139b1f4d13e2ba4d
hyn <mrsay132@gmail.com>
2021-05-26 22:09:56 +0700

    check value % x trong đoạn [a,b]

--- N1022C-percentX.cpp
+++ N1022C-percentX.cpp
@@ -2,13 +2,10 @@
 #include <iostream>
 using namespace std;
 long long a, b, x;
-int main()
-{
-
+int main() {
     cin >> a >> b >> x;
     long long dem = (b / x) - (a / x);
-    if (a % x == 0)
-    {
+    if (a % x == 0) {
         dem++;
     }
     cout << dem;
