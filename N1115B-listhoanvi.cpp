3f1ae122d8742ca345bd918d7270eef273b847dd
hyn <mrsay132@gmail.com>
2021-05-26 22:06:45 +0700

    liệt kê các hoán vị của n ptX

--- N1115B-listhoanvi.cpp
+++ N1115B-listhoanvi.cpp
@@ -5,33 +5,26 @@ using namespace std;
 int n;
 int Bool[MAX] = { 0 };//Đánh dấu chưa có phần tử nào sử dụng hết
 int A[MAX];//Lưu hoán vị vào mảng A
-void xuat()
-{
-    for (int i = 1; i <= n; i++)
-    {
+void xuat() {
+    for (int i = 1; i <= n; i++) {
         cout << A[i] << " ";
     }
     cout << endl;
 }
-void Try(int k)
-{
-    for (int i = 1; i <= n; i++)
-    {
-        if (!Bool[i])    //Kiểm tra nếu phần tử chưa được chọn thì sẽ đánh dấu
-        {
+void Try(int k) {
+    for (int i = 1; i <= n; i++) {
+        if (!Bool[i]) { //Kiểm tra nếu phần tử chưa được chọn thì sẽ đánh dấu
             A[k] = i; // Lưu một phần tử vào hoán vị
             Bool[i] = 1;//Đánh dấu đã dùng
             if (k == n) xuat(); //Kiểm tra nếu đã chứa một hoán vị thì xuất
-            else
-            {
+            else {
                 Try(k + 1);
             }
             Bool[i] = 0;
         }
     }
 }
-int main()
-{
+int main() {
     cin >> n;
     Try(1);
 }
