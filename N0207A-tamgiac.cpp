17bf4fe6635002dfca8fd76bbf01a84c13f279eb
hyn <mrsay132@gmail.com>
2021-05-26 20:58:32 +0700

    check hình tam giác

--- N0207A-tamgiac.cpp
+++ N0207A-tamgiac.cpp
@@ -2,14 +2,10 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-int main()
-{
+int main() {
     long long a,b,c;
     cin>>a>>b>>c;
-    if(a+b > c && a+c > b && b+c > a) 
-    {
-        cout<<"YES";
-    }
+    if(a+b > c && a+c > b && b+c > a) cout<<"YES";
     else cout<<"NO";
     return 0;
 }
