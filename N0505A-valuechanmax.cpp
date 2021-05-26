99c5095877a5d0d6839fe68d987249b7a0c03e9e
hyn <mrsay132@gmail.com>
2021-05-26 20:40:29 +0700

    in ra value chẵn max trong ma trận

--- N0505A-valuechanmax.cpp
+++ N0505A-valuechanmax.cpp
@@ -3,25 +3,15 @@
 using namespace std;
 long long n;
 int a[1600][1600];
-int main()
-{
+int main() {
     cin>>n;
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<n; j++)
-        {
-            cin>>a[i][j];
-        }
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) cin>>a[i][j];
     }
     int max = a[0][0];
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<n; j++)
-        {
-            if(a[i][j]%2==0 && a[i][j] > max)
-            {
-                max=a[i][j];
-            }
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if(a[i][j]%2==0 && a[i][j] > max) max=a[i][j];
         }
     }
     cout<<max;
