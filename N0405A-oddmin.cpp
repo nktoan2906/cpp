54c1497706d11ebfdafea0589681b1c8db85765b
hyn <mrsay132@gmail.com>
2021-05-27 14:05:58 +0700

    in ra GTLẺMIN trong mảng

--- N0405A-oddmin.cpp
+++ N0405A-oddmin.cpp
@@ -1,19 +1,15 @@
 //http://laptrinhphothong.vn/Problem/Details/4594
 #include <iostream>
 using namespace std;
-int main()
-{
+int main() {
 	long long n, a, min, max;
 	cin >> n;
 	min = -1;
 	bool minInit = false;
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < n; i++) {
 		cin >> a;
-		if (a % 2 != 0)
-		{
-			if (!minInit || a < min)
-			{
+		if (a % 2 != 0) {
+			if (!minInit || a < min) {
 				minInit = true;
 				min = a;
 			}
