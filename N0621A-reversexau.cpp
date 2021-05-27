e5825c0cd548a16f4f1073bb3894c8ca8e3c039a
hyn <mrsay132@gmail.com>
2021-05-27 13:37:00 +0700

    đảo ngược xâu vừa nhập

--- N0621A-reversexau.cpp
+++ N0621A-reversexau.cpp
@@ -2,12 +2,10 @@
 #include <iostream>
 #include <algorithm> 
 using namespace std; 
-int main() 
-{ 
+int main()  { 
     string str ;
 	getline(cin,str);
     reverse(str.begin(), str.end()); 
-  
     cout << str; 
     return 0; 
 }
