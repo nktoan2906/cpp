32e83e84cdea96818286e902f483bbc7d074376f
hyn <mrsay132@gmail.com>
2021-05-27 13:45:08 +0700

    in ra độ dài xâu

---
+++ N0601A-dodaixau.cpp
@@ -0,0 +1,13 @@
+//http://laptrinhphothong.vn/Problem/Details/4621
+#include <iostream>
+#include<string.h>
+using namespace std;
+char s[101];
+int len;
+int main() {
+    gets(s);
+    len = strlen(s);
+    cout <<len;
+    return 0;
+}
+    
