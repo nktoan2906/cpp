11f9c17d5384968370b90127113046259e61fcaa
hyn <mrsay132@gmail.com>
2021-05-27 13:29:41 +0700

    in ra BCMIN của tổng các chữ số của 2 số nhập vào

--- N0813A-bcmincuasumn.cpp
+++ N0813A-bcmincuasumn.cpp
@@ -8,8 +8,7 @@ ll sumchuso(ll n){
     if(n < 10) return n;
     return n%10 + sumchuso(n/10);
 }
-int main()
-{
+int main() {
     cin>>s1>>s2;
     cout << sumchuso(s1) / __gcd(sumchuso(s1), sumchuso(s2)) * sumchuso(s2)  ;
     return 0;
