69b4fa0080c04c15fe4aeab0c99bbde9aac70cde
hyn <mrsay132@gmail.com>
2021-06-05 12:31:42 +0700

    in ra số nhỏ nhì

--- N0213B-secondmin.cpp
+++ N0213B-secondmin.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 #define sui short unsigned int
+#define RUNO3() for (sui i = 0; i < 3; i++) 
 using namespace std;
 int main() {
-	int a, b, min, runner;
+	int a, b, min2, cde;
 	cin>>a>>b;
 	if (a < b) {
 		a += b;
 		b = a - b;
 		a -= b;
 	}
-	min = a;
-	for (sui i = 0; i < 3; i++) {
-		cin>>runner;
-		if (runner < b) {
-			min = b;
-			b = runner;
+	min2 = a;
+	RUNO3(){
+		cin>>cde;
+		if (cde < b) {
+			min2 = b;
+			b = cde;
 		}
-		if (runner > b && runner < min) min = runner;
+		if (cde > b && cde < min2) min2 = cde;
 	}
-	cout<<min;
+	cout<<min2;
 	return 0;
 }
