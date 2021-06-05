a9282a89c9d4897403d659dd8f23536843be1550
hyn <mrsay132@gmail.com>
2021-06-05 15:05:25 +0700

    liệt kê, đặt n mở và đóng ngoặc sao cho hợp lệ

---
+++ N1109D.Ngoac.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+char s[22];
+int n, dem, L, R;
+void Cout() {
+	for (int i = 1; i <= n; ++i){
+		cout << s[i];
+	}
+	cout << '\n';
+}
+void ListNgoac(int i) {
+	if (i > n) {
+		dem++;
+		Cout();
+		return;
+	}
+	else {
+		if (L < n / 2) {
+			s[i] = '(';
+			L++;
+			ListNgoac(i + 1);
+			L--;
+		}
+		if (L > R) {
+			s[i] = ')';
+			R++;
+			ListNgoac(i + 1);
+			R--;
+		}
+	}
+}
+int main() {
+	cin >> n;
+	n *= 2;
+	ListNgoac(1);
+	return 0;
+}
