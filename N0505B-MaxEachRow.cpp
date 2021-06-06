c1c667f20bed48a619acd1053949bab027812614
hyn <mrsay132@gmail.com>
2021-06-06 14:10:13 +0700

    in ra max từng hàng

--- N0505B-MaxEachRow.cpp
+++ N0505B-MaxEachRow.cpp
@@ -1,19 +1,19 @@
 #include <bits/stdc++.h>
 #define ll long long
-#define FOR1(i,k) for (i = 0; i < k; i++)
-#define FOR(i,k,n) for (i = 0; i < k; i = i + n)
+#define FOR1(i,mn) for (i = 0; i < mn; i++)
+#define FOR(i,mn,n) for (i = 0; i < mn; i += n)
 using namespace std;
 int main() {
 	ll m, n, i, t;
 	cin >> m >> n;
 	vector<ll> a;
-	ll k = m * n;
-	FOR1(i,k){
+	ll mn = m * n;
+	FOR1(i,mn){
 		cin >> t;
 		a.push_back(t);
 	}
-	FOR(i,k,n){
-		vector<ll>::iterator c = max_element(a.begin() + i, a.begin() + i + n);
-		cout << *c << endl;
+	FOR(i,mn,n){
+		vector<ll>::iterator kq = max_element(a.begin() + i, a.begin() + i + n);
+		cout << *kq << endl;
 	}
 }
