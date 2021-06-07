eec1c4732038af645ad90ed524c9575ac9da3085
hyn <mrsay132@gmail.com>
2021-06-07 23:25:11 +0700

    in ra hình vuông dấu $

--- N0304A-daudollar.cpp
+++ N0304A-daudollar.cpp
@@ -1,11 +1,12 @@
-//http://laptrinhphothong.vn/Problem/Details/4574
 #include <iostream>
+#define FOR(i,n) for (i = 1; i <= n; ++i) 
+#define FOR(j,n) for (j = 1; j <= n; ++j) 
 using namespace std;
 int main() {
-    int a, i, j;
-    cin >> a;
-    for (i = 1; i <= a; i++) {
-        for (j = 1; j <= a; j++) {
+    int n, i, j;
+    cin >> n;
+    FOR(i,n){
+        FOR(j,n) {
             cout << "$"; //luu y,co mot khoang trong o day
         }
         cout << "\n";
