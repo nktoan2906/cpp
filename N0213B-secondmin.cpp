cc72dabb273d4839a7f9c0e62e5e451eaf3fb9ed
hyn <mrsay132@gmail.com>
2021-06-07 15:08:21 +0700

    in ra min2

--- N0213B-secondmin.cpp
+++ N0213B-secondmin.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 #define sui short unsigned int
 #define RUNO3() for (sui i = 0; i < 3; i++) 
+#define min_nho_cde_nho_min2 cde > min && cde < min2
+#define SWAP(min2,cde) min2 = min; min = cde;
 using namespace std;
 int main() {
-	int a, b, min2, cde;
-	cin>>a>>b;
-	if (a < b) {
-		a += b;
-		b = a - b;
-		a -= b;
+	int max, min, min2, cde;
+    cin>>max>>min;
+	if (max < min) {                 //- Nếu số nhập vào này lớn hơn max thì số này là max còn giá trị cần tìm lớn nhì sẽ là max hiện tại
+		max += min;                 //- Nếu số nhập vào này nhỏ hơn min thì số này là min còn giá trị còn tìm nhỏ nhì sẽ là min hiện tại
+		min = max - min;           //- Nếu số nhập vào này nhỏ hơn max và lớn hơn giá trị lớn nhì hiện lại (ở lần đầu thì giá trị lớn nhì là min) thì nó sẽ là giá trị lớn nhì --> gán lớn nhì là giá trị nhập vào
+		max -= min;               //- Tương tự với nhỏ nhì
 	}
-	min2 = a;
+	min2 = max;
 	RUNO3(){
 		cin>>cde;
-		if (cde < b) {
-			min2 = b;
-			b = cde;
-		}
-		if (cde > b && cde < min2) min2 = cde;
+		if (cde < min) SWAP(min2,cde);
+		if (min_nho_cde_nho_min2) min2 = cde;
 	}
 	cout<<min2;
-	return 0;
+	return 0;                                                                     
 }
