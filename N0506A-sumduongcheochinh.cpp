8f82efd0ff29454735f176ccbb5f1e66ed7787b0
hyn <mrsay132@gmail.com>
2021-06-07 14:26:42 +0700

    tính tổng trên đường chéo chính

--- N0506A-sumduongcheochinh.cpp
+++ N0506A-sumduongcheochinh.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
+#define FOR(i,n) for(int i=1;i<=n;++i)
+#define FOR(j,n) for(int j=1;j<=n;++j)
 using namespace std;
 const int N = 101;
 int main() {
-    int a[N][N],n,s = 0;
+    int a[N][N],n;
+	double s = 0;
     cin>>n;
-	for(int i=1;i<=n;++i){
-		for(int j=1;j<=n;++j) cin>>a[i][j];
+	FOR(i,n){
+		FOR(j,n) cin>>a[i][j];
 	} 
-    for(int i=1;i<=n;i++) s+=a[i][i];
-    cout<<s;
+    FOR(i,n){
+		s+=a[i][i];
+	}
+	cout<<s;
 }
