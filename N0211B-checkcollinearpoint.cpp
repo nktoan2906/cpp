306b6847074fd6b132e40fc3e1cc99b90d42e7ea
hyn <mrsay132@gmail.com>
2021-06-07 14:43:05 +0700

    check đ A,B,C có thẳng hàng hay ko

--- N0211B-checkcollinearpoint.cpp
+++ N0211B-checkcollinearpoint.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <math.h>
-#include <stdlib.h>
 using namespace std;
 void Collinear(int x1, int y1, int x2, int y2, int x3, int y3) {
     int a = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2);
