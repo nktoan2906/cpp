587618cbfb779d794e94df08d200e053a57a6c90
hyn <mrsay132@gmail.com>
2021-06-07 15:12:23 +0700

    check số dv có chẵn hay ko

--- N0218B-chanofdv.cpp
+++ N0218B-chanofdv.cpp
@@ -1,11 +1,9 @@
-//http://laptrinhphothong.vn/Problem/Details/5756
 #include <iostream>
 using namespace std;
 int main (){
     long long n;
     cin>>n;
-    long long nsau = n%10;
-    if(nsau %2 == 0)cout<<"YES";
+    if((n%10) %2 == 0) cout<<"YES";
     else cout<<"NO";
     return 0;
 }
