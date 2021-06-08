33b0ffe2890e82d76489cdea12400d071d727baa
hyn <mrsay132@gmail.com>
2021-06-08 13:00:38 +0700

    list hoán vị của nptX

--- N1115B-listhoanvi.cpp
+++ N1115B-listhoanvi.cpp
@@ -1,31 +1,30 @@
-//http://laptrinhphothong.vn/Problem/Details/5740
 #include<iostream>
-#define MAX 20
+#define FOR(i,n) for (int i = 1; i <= n; i++)
+#define max 20
 using namespace std;
-int n;
-int Bool[MAX] = { 0 };//Đánh dấu chưa có phần tử nào sử dụng hết
-int A[MAX];//Lưu hoán vị vào mảng A
-void xuat() {
-    for (int i = 1; i <= n; i++) {
-        cout << A[i] << " ";
+int n,kt[max] = { 0 };//Đánh dấu chưa có phần tử nào sử dụng hết
+int a[max];//Lưu hoán vị vào mảng a
+void COUT() {
+    FOR(i,n) {
+        cout << a[i] << " ";
     }
     cout << endl;
 }
-void Try(int k) {
-    for (int i = 1; i <= n; i++) {
-        if (!Bool[i]) { //Kiểm tra nếu phần tử chưa được chọn thì sẽ đánh dấu
-            A[k] = i; // Lưu một phần tử vào hoán vị
-            Bool[i] = 1;//Đánh dấu đã dùng
-            if (k == n) xuat(); //Kiểm tra nếu đã chứa một hoán vị thì xuất
+void ListHoanVi(int k) {
+    FOR(i,n) {
+        if (!kt[i]) { //Kiểm tra nếu phần tử chưa được chọn thì sẽ đánh dấu
+            a[k] = i; // Lưu một phần tử vào hoán vị
+            kt[i] = 1;//Đánh dấu đã dùng
+            if (k == n) COUT(); //Kiểm tra nếu đã chứa một hoán vị thì xuất
             else {
-                Try(k + 1);
+                ListHoanVi(k + 1);
             }
-            Bool[i] = 0;
+            kt[i] = 0;
         }
     }
 }
 int main() {
     cin >> n;
-    Try(1);
+    ListHoanVi(1);
 }
     
