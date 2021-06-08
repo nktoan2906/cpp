665f1c072f20321f43bce61391749f70b56cd219
hyn <mrsay132@gmail.com>
2021-06-08 07:28:05 +0700

    tính Fn

--- N0316C-F.cpp
+++ N0316C-F.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #define FOR3(i,n) for (int i = 3; i <= n; ++i)
-#define a1a2_1 a[1] = 1; a[2] = 1;
+#define F1F2_1 F[1] = 1; F[2] = 1;
 using namespace std;
 const int N = 1e6;
 const int mod = 1e9 + 7;
-long long n, a[N];
+long long n, F[N];
 int main() {
-	a1a2_1;
+	F1F2_1;
 	cin >> n;
 	FOR3(i,n){
-		a[i] = ((a[i - 1]%mod) + (a[i - 2]%mod)) % mod;
+		F[i] = ((F[i - 1]%mod) + (F[i - 2]%mod)) % mod;
 	}
-	cout << a[n];
+	cout << F[n];
 	return 0;
 }
