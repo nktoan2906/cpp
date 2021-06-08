41dcea4eab7513bb311a721b8c38f149691848c1
hyn <mrsay132@gmail.com>
2021-06-08 07:32:38 +0700

    check xem n có thuộc dãy từ 2 -> n hay ko(i+=3)

--- N0318B-sumdayso.cpp
+++ N0318B-sumdayso.cpp
@@ -1,14 +1,14 @@
-//http://laptrinhphothong.vn/Problem/Details/5738
 #include <iostream>
+#define FOR2TANG3(i,n) for(int i=2;i<=n;i+=3)
 using namespace std;
-long long n,s,d;
+int n,s,d;
 int main() {
 	cin>>n;
 	s = 2;
 	while (s < n){
 		s+=3;
 		if(s == n){
-			for(int i=2;i<=n;i+=3){
+			FOR2TANG3(i,n) {
 				d+=i;
 			}
 			cout<<d;
