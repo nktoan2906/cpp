07aaf91aab34cdd9bb642bcac7c135abafc8bd24
hyn <mrsay132@gmail.com>
2021-06-08 07:59:19 +0700

    tính sum ước của n

--- N0325C-sumuoc.cpp
+++ N0325C-sumuoc.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #define ull unsigned long long
+#define FOR(i,n) for (ull i = 1; i*i <= n; i++)
 #define nmodi n % i == 0 
 using namespace std;
 int main() {
 	ull n,s = 0;
 	cin >> n;
-	for (ull i = 1; i*i <= n; i++) {
+	FOR(i,n){
 		if (nmodi) {
 			ull j = n / i;
 			if (i == j) s += i;
