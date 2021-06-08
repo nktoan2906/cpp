e50b600234e7e1d40d96a6f8bd115d6383a938fb
hyn <mrsay132@gmail.com>
2021-06-08 11:47:19 +0700

    tính sum chẵn bình phương trong mảng

--- N0346C-sumchanbac2inarr.cpp
+++ N0346C-sumchanbac2inarr.cpp
@@ -1,16 +1,15 @@
-//http://laptrinhphothong.vn/Problem/Details/5915
 #include <iostream>
+#define FOR(i,n) for(int i=1;i<=n;i++)
 using namespace std;
 int main () {
-    long long n;
+    int n;
     int a[10000+1];
     cin>>n;
-    for(int i=1;i<=n;i++) cin>>a[i];
-    for(int i=1;i<=n;i++) {
-        long long s = (4 * a[i] * (a[i] + 1) * (2 * a[i] + 1)) / 6;
+    FOR(i,n) cin>>a[i];
+    FOR(i,n)  {
+        int s = (4 * a[i] * (a[i] + 1) * (2 * a[i] + 1)) / 6;
         cout<<s<<endl;
     }
     return 0;
-    //( 4 * n * ( n + 1 ) * ( 2 * n + 1 ))/6;
 }
     
