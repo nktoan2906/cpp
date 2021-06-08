bec69313ac553db260b425cbe00cdee14869ac63
hyn <mrsay132@gmail.com>
2021-06-08 08:01:03 +0700

    check xem ếch có tới đc cụm relax hay ko ?

---
+++ N0327B-Frog.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, k;
+    cin >> n >> k;
+    while (k % n != 0) {
+        int m = k % n;
+        k = m + n;
+        n = m;
+    }
+    cout << n;
+}
+    
