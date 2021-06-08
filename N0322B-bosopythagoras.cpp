1f890650b511f136937a5cc722779731aeb19373
hyn <mrsay132@gmail.com>
2021-06-08 07:54:50 +0700

    in bộ số Pythagoras mà cả 3 đều <= n

--- N0322B-bosopythagoras.cpp
+++ N0322B-bosopythagoras.cpp
@@ -1,20 +1,22 @@
-//http://laptrinhphothong.vn/Problem/Details/5765
 #include <iostream>
 #include <cmath>
+#define ll long long
+#define FOR1(i,n) for (int i=1;i<=n;++i) 
+#define FOR(j,i,n) for (int j=i+1;j<=n;j++) 
+#define can_ii_plus_jj sqrt(i*i+j*j);
 using namespace std;
 int main() {
-    long long n,x,y,i,j,k,l,tong;
+    ll n;
 	cin>>n;
-	long long a=1;
-	long long b=0;
+	ll a=1,b=0;
 	if (n<5) cout<<-1;
 	else {
-	    for (i=1;i<n;i++) {
-	        for (j=i+1;j<n;j++) {
-                double z=sqrt(i*i+j*j);
+        FOR1(i,n) {
+	        FOR(j,i,n) {
+                double z= can_ii_plus_jj;
                 if((int)z==z && ( z<n || z==n)) cout<<i<<" "<<j<<" "<<(int)z<<endl;
 	        }
-	     }
+	    }
 	}
     return 0;
 }
