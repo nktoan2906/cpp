e9ff70028f0be938344aea7c19be0cb47efc554e
hyn <mrsay132@gmail.com>
2021-06-08 07:44:25 +0700

    check số Armstrong

--- N0321B-armstrong.cpp
+++ N0321B-armstrong.cpp
@@ -1,22 +1,19 @@
-//http://laptrinhphothong.vn/Problem/Details/5764
 #include <iostream>
 #include <stdio.h>
 #include <math.h>
+#define FOR(i,SoChuSo) for(int i = 1; i <= SoChuSo ; ++i)
 using namespace std;
 int main() {
     int n;
     while(cin >> n) {
-        int S = 0;
-        int SoChuSo = 0;
-        int p = n;
-        int q = n;
+        int S = 0,SoChuSo = 0,p = n,q = n;
         while(n>0) {
-            n=n/10;
+            n /= 10;
             SoChuSo += 1;
         }
-        for(int l = 1; l <= SoChuSo ; l++) {
+        FOR(i,SoChuSo) {
             int m = p % 10;
-            p = p / 10;
+            p /= 10;
             S += pow(m, SoChuSo);
         }
         if (S == q) cout << "YES" ;
