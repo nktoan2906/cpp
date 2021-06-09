a7d20470ff3e7354cf4ab228bffaadb115045ce3
hyn <mrsay132@gmail.com>
2021-06-09 07:28:08 +0700

    tính a mũ n bằng chia để trị

--- N1101A-amun.cpp
+++ N1101A-amun.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#define ble b % 2 == 0
 #define ll long long 
 using namespace std;
 const int mod = 1e9 + 7;
-ll Mu(ll a, ll b) {
+inline ll Mu(ll a, ll b) {
     if (b == 0) return 1;
     ll kq = Mu(a, b / 2);
     kq = (kq * kq) % mod;
-    if (b % 2 == 1) kq = (kq * a) % mod;
+    if (ble) kq = (kq * a) % mod;
     return kq;
 }
 int main() {
