600aa685f4c6c60e19eeac05b94b213ab4366a70
hyn <mrsay132@gmail.com>
2021-06-09 07:54:47 +0700

    check số vui vẻ

--- N0811B-happynumber.cpp
+++ N0811B-happynumber.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #define ll long long
 #define ull unsigned long long
+#define happynumchan happynum % 2 == 0
+#define FOR(a,b) for (ll i = a; i <= b; ++i) 
 ull a, b, dem, happynum, chusocuan;
 using namespace std;
 int main() {
     cin >> a >> b;
-    for (long long i = a; i <= b; i++) {
+    FOR(a,b){
         chusocuan = i;
         happynum = 0;
         while (chusocuan != 0) {
             happynum += ((chusocuan % 10) * (chusocuan % 10));
             chusocuan /= 10;
         }
-        if (happynum % 2 == 0) dem++;
+        if (happynumchan) dem++;
     }
     cout<<dem;
 }
