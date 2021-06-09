e95b516f84c606fffaa3f5904fa085ddbbd88be6
hyn <mrsay132@gmail.com>
2021-06-09 08:07:17 +0700

    tìm SNTMAX < n

--- N0807B-maxsnt.cpp
+++ N0807B-maxsnt.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
-#include <math.h>
+#define FORCAN2(i,n) for(int i = 2; i * i <= n ; i++)
+#define nmodi n % i == 0
 using namespace std;
-bool is_prime(int n) {
-    bool kt = true;
-    for (int i = 2; i <= sqrt(n); i++) {
-        if (n % i == 0) {
-            kt = false;
-            break;
-        }
+bool SNT(int n){
+    if(n < 2) return false;
+    FORCAN2(i,n) {
+        if(nmodi) return false;
     }
-    return kt;
+    return true;
 }
-int main(){
+int main() {
     int n,x = 0;
     cin >> n;
     n--;
     while (x == 0) {
-        if (is_prime(n)) x = n;
+        if (SNT(n)) x = n;
         else n--;
     }
     cout << x;
