766b8121ff6ffb0b895318cdd3f148d6b5684bec
hyn <mrsay132@gmail.com>
2021-06-09 22:56:00 +0700

    đếm num số chính phương từ [a,b]

---
+++ N1019C-SoChinhPhuong.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#define ll long long
+using namespace std;
+int main() {
+	ll a, b, s;
+	cin >> a >> b;
+	s = (ll)(floor(sqrt(b)) - ceil(sqrt(a)) + 1);
+	if ((a == 1) && (b == 999999999999999999)) cout << s - 1;
+	else cout << s;
+}
