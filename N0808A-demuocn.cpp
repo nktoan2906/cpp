4e118a56a2dd8316dd556b081a6d90fa19d09f91
hyn <mrsay132@gmail.com>
2021-06-09 08:05:51 +0700

    đếm số ước của n

--- N0808A-demuocn.cpp
+++ N0808A-demuocn.cpp
@@ -1,13 +1,12 @@
-//http://laptrinhphothong.vn/Problem/Details/5823
 #include <iostream>
-#include <math.h>
 #define ll long long
+#define nmodi n % i == 0
 using namespace std;
 int main() {
       ll n, i = 1, dem = 0;
       cin >> n;
-      while (i <= sqrt(n)) {
-            if (n % i == 0) {
+      while (i * i <= n) {
+            if (nmodi) {
                   dem++;
                   if (i != (n / i)) dem++;
             }
