c5280a98ea60e4042998a102343ae1dc566bc090
hyn <mrsay132@gmail.com>
2021-06-09 08:13:03 +0700

    check if (pi + pj) chia hết cho 7

--- N0805A-souocmod7.cpp
+++ N0805A-souocmod7.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 #define ll long long
+#define FOR(i,n) for(int i=1;i<=n;++i)
+#define FOR_1(i,n) for(int i=1;i<=n-1;++i)   
+#define FORPLUS1(j,i,n) for(int j=i+1;j<=n;j++)
 using namespace std;
+const int N = 101;
 ll souoc(ll n){
     ll dem = 0;
-    for(int i=1;i<=n;i++){
+    for(int i=1;i<=n;++i){
         if(n%i==0) dem++;
     }
     return dem;
 }
 int main() {
-    ll n,a[101],dem = 0,p[101];
+    ll n,a[N],dem = 0,p[N];
     cin>>n;
-    for(int i=1;i<=n;i++) cin>>a[i];
-    for(int i=1;i<=n;i++) p[i]=souoc(a[i]);
-    for(int i=1;i<=n-1;i++){
-        for(int j=i+1;j<=n;j++){
-            if((p[i] + p[j]) % 7 == 0) dem++;
+    FOR(i,n) cin>>a[i];
+    FOR(i,n) p[i]=souoc(a[i]);
+    FOR_1(i,n){
+        FORPLUS1(j,i,n) {
+            if((p[i] + p[j]) % 7 == 0) ++dem;
         }
     }
     cout<< dem;
