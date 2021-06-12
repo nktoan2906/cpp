b3d481e7e2b75b25464c99a38c342b3f86224531
hyn <mrsay132@gmail.com>
2021-06-12 07:01:23 +0700

    đếm số cặp TM lcm(ai,aj) <= k

--- N0804A-demlcm.cpp
+++ N0804A-demlcm.cpp
@@ -1,17 +1,19 @@
 #include<iostream>
 #include <math.h>
 #include <algorithm>
-#define int long long
+#define ll long long
 #define lcm(a,b) a*b / __gcd(a, b)
+#define FOR1(i,n) for(int i = 1; i <= n; i++)
+#define FOR(j,i,n) for(int j = i + 1; j <= n; j++)
 using namespace std;
-const int N = 1e2 + 3;
-int a[N],dem;
+const ll N = 1e2 + 3;
+ll a[N],dem;
 signed main() {
-    int n, k;
+    ll n, k;
     cin >> n >> k;
-    for(int i = 1; i <= n; i++) cin >> a[i];
-    for(int i = 1; i < n; i++) {
-		for(int j = i + 1; j <= n; j++){
+    FOR1(i,n) cin >> a[i];
+    FOR1(i,n) {
+		FOR(j,i,n) {
             if(lcm(a[i], a[j]) <= k && lcm(a[i], a[j]) > 0) dem++;
 		}
 	}
