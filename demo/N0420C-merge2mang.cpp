6e6f45a49bf4dd5192fd43ed338c3dbfb0e6e0c4
hyn <mrsay132@gmail.com>
2021-06-13 22:50:36 +0700

    gộp 2 mảng làm 1 r sort giảm

--- demo/N0420C-merge2mang.cpp
+++ demo/N0420C-merge2mang.cpp
@@ -1,7 +1,7 @@
+//http://laptrinhphothong.vn/Problem/Details/4608
 #include <iostream>
 #include <math.h>
 #include <algorithm>
-//http://laptrinhphothong.vn/Problem/Details/4608
 #define FOR(i,n) for (int i = 0; i < n; ++i)
 using namespace std;
 int main() {
@@ -13,13 +13,13 @@ int main() {
    FOR(i,n) cin >> b[i];
    FOR(i,n) {     //them mang a vao mang c
       c[index] = a[i];
-      index++;
+      ++index;
    }
    FOR(i,n) { // them mang b vao mang c
       c[index] = b[i];
-      index++;
+      ++index;
    }
-   sort(c,c+ 2 * n);
+   sort(c,c+ 2 * n,greater<int>());
    for (int i = 0; i < 2 * n; ++i) cout << c[i] << " ";
    return 0;
 }
