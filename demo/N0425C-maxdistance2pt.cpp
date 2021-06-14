f17dc2a1425c3edcd0ad395ab220f839f1aabcc7
hyn <mrsay132@gmail.com>
2021-06-14 07:35:34 +0700

    chưa AC đc bài

--- demo/N0425C-maxdistance2pt.cpp
+++ demo/N0425C-maxdistance2pt.cpp
@@ -1,3 +1,4 @@
+//http://laptrinhphothong.vn/Problem/Details/5654
 #include <iostream>
 #include <math.h>
 #include <algorithm>
@@ -5,10 +6,10 @@
 using namespace std;
 int main()
 {
-    ll n,dem = 0;
+    ll n;
     cin>>n;
-    ll a[n];
-    for(ll i=0;i<n;++i) cin>>a[i];
+    ll a[n],dem = 0;
+    for(int i=0;i<n;++i) cin>>a[i];
     sort(a,a+n);
     cout<<a[n - 1] - a[0];
     return 0;
