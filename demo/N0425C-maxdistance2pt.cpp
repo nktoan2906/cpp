13831398b9b94a4fe392634596efd9ab0a2e46dd
hyn <mrsay132@gmail.com>
2021-06-14 07:32:32 +0700

    chưa AC đc bài

--- demo/N0425C-maxdistance2pt.cpp
+++ demo/N0425C-maxdistance2pt.cpp
@@ -3,14 +3,14 @@
 #include <algorithm>
 #define ll long long
 using namespace std;
-const int N = 1e6;
-ll a[N],n,dem = 0;
 int main()
 {
+    ll n,dem = 0;
     cin>>n;
-    for(ll i=1;i<=n;i++) cin>>a[i];
-    sort(a+1,a+n+1);
-    cout<<a[n] - a[1];
+    ll a[n];
+    for(ll i=0;i<n;++i) cin>>a[i];
+    sort(a,a+n);
+    cout<<a[n - 1] - a[0];
     return 0;
 }
     
