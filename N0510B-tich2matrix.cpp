78da1a0c9e2c1a7fecd8c391524520e5f9b5a6ae
hyn <mrsay132@gmail.com>
2021-06-16 15:30:34 +0700

    tính tích 2 matrix

--- N0510B-tich2matrix.cpp
+++ N0510B-tich2matrix.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
+#define FOR(i,m) for (int i = 0; i < m; ++i)
+#define FOR(j,n) for (int j = 0; j < n; ++j)
+#define FOR(i,n) for (int i = 0; i < n; ++i)
+#define FOR(j,p) for (int j = 0; j < p; ++j)
+#define FOR(k,n) for(int k = 0; k < n; ++k)
 using namespace std;
 int main() {
    int n, m, p;
    cin >> m >> n >> p;
    int a[m][n], b[n][p], c[m][p];
-   for (int i = 0; i < m; ++i) {
-      for (int j = 0; j < n; ++j) cin >> a[i][j];
+   FOR(i,m) {
+      FOR(j,n) cin >> a[i][j];
    }
-   for (int i = 0; i < n; ++i) {
-      for (int j = 0; j < n; ++j) cin >> b[i][j];
+   FOR(i,n) {
+      FOR(j,p) cin >> b[i][j];
    }
-   for (int i = 0; i < m; ++i) {
-      for (int j = 0; j < p; ++j) {
+   FOR(i,m) {
+      FOR(j,p) {
          c[i][j] = 0;
-         for(int k = 0; k < n; ++k) c[i][j] += (a[i][k] * b[k][j]);
+         FOR(k,n) c[i][j] += (a[i][k] * b[k][j]);
       }
    }
-   for (int i = 0; i < m; ++i) {
-      for (int j = 0; j < p; ++j) {
+   FOR(i,m) {
+      FOR(j,p) {
           cout<<c[i][j]<<" ";
       }
       cout<<"\n";
