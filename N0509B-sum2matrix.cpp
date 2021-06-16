0c9c72b61240a63597d981492326cea97b70f142
hyn <mrsay132@gmail.com>
2021-06-16 15:25:51 +0700

    tính sum 2 matrix

--- N0509B-sum2matrix.cpp
+++ N0509B-sum2matrix.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
+#define FOR(i,n) for (int i = 0; i < m; ++i)
+#define FOR(j,n) for (int j = 0; j < n; ++j)
 using namespace std;
 int main() {
     int n, m;
     cin >> m >> n;
     int a[m][n], b[m][n], c[m][n];
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < n; ++j) cin >> a[i][j];
+    FOR(i,n) {
+        FOR(j,n) cin >> a[i][j];
     }
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < n; ++j) cin >> b[i][j];
+    FOR(i,n) {
+        FOR(j,n) cin >> b[i][j];
     }
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < n; ++j) c[i][j] = a[i][j] + b[i][j];
+    FOR(i,n) {
+        FOR(j,n) c[i][j] = a[i][j] + b[i][j];
     }
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < n; ++j) {
+    FOR(i,n) {
+        FOR(j,n) {
             cout << c[i][j] << " ";
         }
         cout << endl;
