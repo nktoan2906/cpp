fc091fbfc1982424258acaa4298ca0ea33d562ce
hyn <mrsay132@gmail.com>
2021-06-18 08:44:54 +0700

    kế thừa đc bảo vệ basic

--- OOP/modifiler-kethuadcbaove.cpp
+++ OOP/modifiler-kethuadcbaove.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 using namespace std;
-// kế thừa được bảo vệ làm cho public & protected các member của lớp cơ sở 
+// kế thừa được bảo vệ làm cho public & protected các member của lớp cs 
 // protected trong lớp dẫn xuất.
 class Base {
-    private: int pvt = 1;
-    protected: int prot = 2;
-    public: int pub = 3;
+    private: int riengtu = 1;
+    protected: int dcbaove = 2;
+    public: int congkhai = 3;
     int GetPrivate() {
-        return pvt;
+        return riengtu;
     }
 };
 class ProtectedDerived : protected Base {
     public:
     int GetProtected() {
-        return prot;
+        return dcbaove;
     }
     int GetPublic() {
-        return pub;
+        return congkhai;
     }
 };
 int main() {
