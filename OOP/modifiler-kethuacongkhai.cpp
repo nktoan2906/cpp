9d5b8e8ab9e654b03fc1efbc3122124b47e4b1ac
hyn <mrsay132@gmail.com>
2021-06-18 08:42:33 +0700

    kế thừa công khai basic

--- OOP/modifiler-kethuacongkhai.cpp
+++ OOP/modifiler-kethuacongkhai.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 using namespace std;
-// kế thừa công khai làm cho public các members của lớp cơ sở public trong lớp dẫn 
-// xuất và các protected members của lớp cơ sở vẫn protected ở trong lớp dẫn xuất.
+// kế thừa công khai làm cho public các members của lớp cs public trong lớp dẫn 
+// xuất & các protected members của lớp cs vẫn protected ở trong lớp dẫn xuất.
 class Base {
-    private: int pvt = 1;
-    protected: int prot = 2;
-    public: int pub = 3;
+    private: int riengtu = 1;
+    protected: int dcbaove = 2;
+    public: int congkhai = 3;
     int getPVT() {
-        return pvt;
+        return riengtu;
     }
 };
 class PublicDerived : public Base {
     public:
     int getProt() {
-        return prot;
+        return dcbaove;
     }
 };
 int main() {
     PublicDerived object1;
     cout << "Private = " << object1.getPVT() << endl;
     cout << "Protected = " << object1.getProt() << endl;
-    cout << "Public = " << object1.pub << endl;
+    cout << "Public = " << object1.congkhai << endl;
     return 0;
 }
