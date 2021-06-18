d1a09deb08bb86120ee81f5ed7b66d83947b3489
hyn <mrsay132@gmail.com>
2021-06-18 11:49:45 +0700

    constructor

---
+++ OOP/constructor.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+class HS{
+public : 
+    HS(){ //  đây là 1 phương thức khởi tạo 
+        cout<<"Constructor Called";
+    }
+};
+int main(){
+    HS hs;
+}
