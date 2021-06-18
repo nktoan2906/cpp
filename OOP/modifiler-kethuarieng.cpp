51fe656972eeb24a3ef769768aa9edd8a99e9cc0
hyn <mrsay132@gmail.com>
2021-06-18 08:41:46 +0700

    kế thừa riêng basic

--- OOP/modifiler-kethuarieng.cpp
+++ OOP/modifiler-kethuarieng.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
+//kế thừa riêng làm cho public và protected các members của lớp cs private
+//trong lớp dẫn xuất.
 class Base {
     private: int riengtu = 1;
     protected: int dcbaove = 2;
     public: int congkhai = 3;
-    int getPVT() {
+    int GetPrivate() {
         return riengtu;
     }
 };
