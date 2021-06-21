e62c2b8593e4e72d8d00b2a3801f4f2de79808e0
hyn <mrsay132@gmail.com>
2021-06-21 23:23:55 +0700

    tính sum trong đoạn [a,b] mà ko chia hết cho 13

--- PK0033C-boicua13.cpp
+++ PK0033C-boicua13.cpp
@@ -4,9 +4,9 @@ using namespace std;
 ll a, b, s = 0, temp = 0;
 int main() {
    cin >> a >> b;
-   ll X = b / 13, Y = (a - 1) / 13;
-   ll tmp = 13 * (X * (X + 1) / 2);
-   ll tmp2 = 13 * (Y * (Y + 1) / 2);
+   ll tmp = 13 * ((b / 13) * ((b / 13) + 1) / 2);
+   ll tmp2 = 13 * ( ((a - 1) / 13) * ( ((a - 1) / 13) + 1) / 2);
    ll temp = (a + b) * (b - a + 1) / 2;
    cout << temp - (tmp - tmp2);
 }
+    
