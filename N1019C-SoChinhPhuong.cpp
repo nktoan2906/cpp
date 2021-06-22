c0e12d4d49540b31ad41c054f9c36959f5fc20b1
hyn <mrsay132@gmail.com>
2021-06-22 18:44:50 +0700

    check số chính phương trong đoạn [a,b]

--- N1019C-SoChinhPhuong.cpp
+++ N1019C-SoChinhPhuong.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
 	ll a, b, s;
 	cin >> a >> b;
-	s = (ll)(floor(sqrt(b)) - ceil(sqrt(a)) + 1);
+	s = (ll)(sqrt(b) - sqrt(a) + 1);
 	if ((a == 1) && (b == 999999999999999999)) cout << s - 1;
 	else cout << s;
 }
