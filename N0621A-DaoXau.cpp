7e8ba6966c85b02edfc5f5af269eb4dabad7cd57
hyn <mrsay132@gmail.com>
2021-06-23 19:58:50 +0700

    đảo xâu

---
+++ N0621A-DaoXau.cpp
@@ -0,0 +1,11 @@
+//http://laptrinhphothong.vn/Problem/Details/5748
+#include <iostream>
+#include <algorithm> 
+using namespace std; 
+int main()  { 
+    string str ;
+	getline(cin,str);
+    reverse(str.begin(), str.end()); 
+    cout << str; 
+    return 0; 
+}
