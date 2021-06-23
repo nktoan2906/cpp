5d29a8d71a6a6b800909e18af415f54e45abd9ea
hyn <mrsay132@gmail.com>
2021-06-23 10:11:59 +0700

    in ra vị trí của pt max trong mảng

---
+++ N0404B-vtmax.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+const int N = 1e5 + 1;
+int main() {
+	int n,a[N];
+    cin>>n;
+    for(int i=1;i<=n;++i) cin>>a[i];
+    int vtmax = 1,max = a[1];
+    for(int i=1;i<=n;++i){
+        if(a[i] >= max) {
+            max = a[i];
+            vtmax = i;
+        }
+    }
+    cout<<vtmax;
+	return 0;
+}
+    
