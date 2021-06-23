88e4f20267738f2112b20ab36e849d32be7e3b9c
hyn <mrsay132@gmail.com>
2021-06-23 16:58:37 +0700

    check đ A,B,C có thẳng hàng ko

--- N0211B-checkcollinearpoint.cpp
+++ N0211B-checkcollinearpoint.cpp
@@ -8,7 +8,7 @@ void Collinear(int x1, int y1, int x2, int y2, int x3, int y3) {
 }
 int main() {
     long long x1, y1, x2, y2, x3, y3;
-    cin>>x1>>y1>>x2>>y2>>x3>>y3;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
     Collinear(x1, y1, x2, y2, x3, y3);
     return 0;
 }
