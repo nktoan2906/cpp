ba462d5ce53c22f0b804bd0997282be5a6237899
hyn <mrsay132@gmail.com>
2021-06-23 17:07:38 +0700

    in ra hình vuông dollar

--- N0304A-daudollar.cpp
+++ N0304A-daudollar.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#define FOR(i,n) for (i = 1; i <= n; ++i) 
-#define FOR(j,n) for (j = 1; j <= n; ++j) 
+#define FOR(i,n) for (int i = 1; i <= n; ++i) 
+#define FOR(j,n) for (int j = 1; j <= n; ++j) 
 using namespace std;
 int main() {
-    int n, i, j;
+    int n;
     cin >> n;
     FOR(i,n){
         FOR(j,n) {
