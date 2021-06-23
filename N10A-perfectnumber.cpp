225e3017ef40e1ce562d9d2804a8ed4a05af5f40
hyn <mrsay132@gmail.com>
2021-06-23 17:10:15 +0700

    check số hoàn hảo

--- N10A-perfectnumber.cpp
+++ N10A-perfectnumber.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <math.h>
-#define FORTRUNCAN(i, n) for (int i = 1; i <= trunc(sqrt(n)); i++)
+#define FOR(i, cann) for (int i = 1; i <= trunc(sqrt(n)); i++)
 using namespace std;
 int n, s = 0;
 int main() {
     cin >> n;
-    FORTRUNCAN(i, n) {
+    FOR(i, cann) {
         if (n % i == 0) s += i + n / i;
     }
     if (s == 2 * n) cout << "YES";
