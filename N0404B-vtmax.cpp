3e7ed7975f2368d72b187bea2622af49dce6506b
hyn <mrsay132@gmail.com>
2021-06-23 10:15:40 +0700

    in ra vị trí pt max trong mảng

--- N0404B-vtmax.cpp
+++ N0404B-vtmax.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 const int N = 1e5 + 1;
 int main() {
-	int n,a[N];
+	int n;
     cin>>n;
+    int a[n];
     for(int i=1;i<=n;++i) cin>>a[i];
     int vtmax = 1,max = a[1];
     for(int i=1;i<=n;++i){
