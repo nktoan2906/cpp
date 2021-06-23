e54e8077fca76ff06d1488a4d5b8061711b58f4c
hyn <mrsay132@gmail.com>
2021-06-23 17:14:00 +0700

    in tam giác xuôi số 0

--- N0305A-so0.cpp
+++ N0305A-so0.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #define FOR(i,n) for(int i = 1; i <= n; ++i)
-#define FOR(j,i) for(int j = 1; j <= i; ++j)
+#define FOR1(j,i) for(int j = 1; j <= i; ++j)
 #define ll long long 
 using namespace std;
 ll n;
 int main() {
     cin >> n;
     FOR(i,n) {
-        FOR(j,i){
+        FOR1(j,i){
             cout << "0";
         }
         cout << endl;
