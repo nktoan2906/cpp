93c08fdeb8ed70885fd8d995efeac05d1318c143
hyn <mrsay132@gmail.com>
2021-06-23 17:04:57 +0700

    in tam giác # ngược

--- N0303A-dauthang.cpp
+++ N0303A-dauthang.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
-#define FOR13() for (int i=1; i<=3; i++)
-#define FOR(i,n) for (int j=1; j<=n; j++)
+#define FOR(i,n) for (int i=1; i<=3; i++)
+#define FOR1(j,n) for (int j=1; j<=n; j++)
 #define ll long long 
 using namespace std;
 ll n;
 int main(){ 
     cin>>n;
-    FOR13() {
-        FOR(i,n) {
+    FOR(i,n) {
+        FOR(j,n) {
             cout<<"#";
         }
-        n--;
+        --n;
         cout<<endl;
     }
     return 0;
