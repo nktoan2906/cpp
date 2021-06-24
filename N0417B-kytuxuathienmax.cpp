cbeea4e8c4edbced0206d19bd88857cfde916b91
hyn <mrsay132@gmail.com>
2021-06-24 19:36:52 +0700

    in ra ký tự xuất hiện nhiều nhất trong n ký tự

--- N0417B-kytuxuathienmax.cpp
+++ N0417B-kytuxuathienmax.cpp
@@ -1,30 +1,30 @@
-#include<iostream>
+#include <iostream>
 #include <queue>
-#define FOR(i,n) for(int i=1;i<=n;i++)
+#define FOR(i, n) for (int i = 1; i <= n; i++)
+#define faster ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
 using namespace std;
 int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0);
-    int n;
+    faster;
+    int n,a[n];
     cin >> n;
-    int a[n];
     char x[n];
-    FOR(i,n)  {
-        cin >> x[i]; 
+    FOR(i, n) {
+        cin >> x[i];
         a[x[i]]++;
-    } 
+    }
     int max = 0;
-    FOR(i,n) {
-        if(max < a[x[i]]) max = a[x[i]];
+    FOR(i, n){ 
+        if (max < a[x[i]]) max = a[x[i]];
     }
     priority_queue<char, vector<char>, greater<char>> priorityquere;
-    FOR(i,n) {
-        if(a[x[i]] == max) priorityquere.push(x[i]);
+    FOR(i, n) {
+        if (a[x[i]] == max) priorityquere.push(x[i]);
     }
-    char u;
-    while(!priorityquere.empty()) {
-        u = priorityquere.top();
+    char kq;
+    while (!priorityquere.empty())  {
+        kq = priorityquere.top();
         break;
     }
-    cout << u;
+    cout << kq;
     return 0;
 }
