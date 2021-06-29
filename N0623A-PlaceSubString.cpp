8566b48e0f6439deb03c893407c92e7be031f8d4
hyn <mrsay132@gmail.com>
2021-06-29 23:16:59 +0700

    in ra vị trí của s1 trong s

---
+++ N0623A-PlaceSubString.cpp
@@ -0,0 +1,12 @@
+#include <iostream>  
+#include <string.h>  
+using namespace std;
+int main()  
+{  
+    string s,s1;
+	getline(cin,s);
+	getline(cin,s1);
+	size_t found = s.find(s1);
+	if(found != string::npos) cout<< found;
+	else cout<<"NO";
+}  
