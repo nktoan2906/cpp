3742b1b94b92f2e4d6dd68f4ce1cfdf68f05344b
hyn <mrsay132@gmail.com>
2021-06-30 18:01:21 +0700

    Tính (a * b)%c (c <= 1e18)

---
+++ N0131C-Modulo3.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main () {
+    long long a,b,c,s=0;
+    cin>>a>>b>>c;
+    a%=c,b%=c;
+    long long q=(long double)a*b/c,res=(a*b-q*c)%c;
+    cout<<res;
+}
