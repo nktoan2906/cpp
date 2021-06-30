631c195acc9c5bb91f57f6a6be2ae285359f0425
hyn <mrsay132@gmail.com>
2021-06-30 14:25:08 +0700

    đếm num A Giao B

--- N0614C-AGiaoB.cpp
+++ N0614C-AGiaoB.cpp
@@ -1,13 +1,16 @@
 #include<iostream>
-#include <map>
+#include <map> 
+#define FOR(i,s) for(int i = 0 ; i < s.length() ; ++i)
+#define FOR1(i,s,s1) for(int i = 0 ; i < (AGiaoB(s) + AGiaoB(s1)).length() ; ++i)
+#define FOR09(i) for(char i = '0' ; i <= '9' ; ++i) 
 #define ll long long
 #define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
 string AGiaoB(string s) {
 	map<char , ll> count;
-	for(int i = 0 ; i < s.length() ; ++i) ++count[s[i]];
+	FOR(i,s) ++count[s[i]];
 	string kq = "";
-	for(int i = 0 ; i < s.length() ; ++i) {
+	FOR(i,s) {
 		if(count[s[i]] > 0) {
 			kq += s[i];
 			count[s[i]] = 0 ;
@@ -21,8 +24,8 @@ signed main(){
 	cin >> s >> s1;
 	ll kq = 0 ;
 	map<char , ll> dem;
-	for(int i = 0 ; i < (AGiaoB(s) + AGiaoB(s1)).length() ; ++i) ++dem[(AGiaoB(s) + AGiaoB(s1))[i]];
-	for(char i = '0' ; i <= '9' ; ++i) {
+	FOR1(i,s,s1) ++dem[(AGiaoB(s) + AGiaoB(s1))[i]];
+	FOR09(i) {
 		if(dem[i] > 1) ++kq;
 	}
 	cout << kq;
