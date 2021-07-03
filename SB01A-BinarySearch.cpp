f95a1896e5c92facecf76d5d531fa76aa0cdf1a3
hyn <mrsay132@gmail.com>
2021-07-03 08:38:47 +0700

    in ra Y nếu có x trong mảng, else N

--- SB01A-BinarySearch.cpp
+++ SB01A-BinarySearch.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#define FOR(i,n) for (ll i = 1; i <= n; i++)
 using namespace std;
 #define ll long long
 int main() {
 	ll n, x;
 	cin >> n >> x;
 	ll a[n];
-	for (ll i = 1; i <= n; i++) cin >> a[i];
-	for (ll i = 1; i <= n; i++){
+	FOR(i,n) cin >> a[i];
+	FOR(i,n) {
 		if (a[i] == x) {
 			cout << "Y";
 			return 0;
