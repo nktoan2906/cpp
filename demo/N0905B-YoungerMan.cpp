f83c214aeab898a18d57ba302c310cb32b6f5ed4
hyn <mrsay132@gmail.com>
2021-07-05 22:47:05 +0700

    in ra cầu thủ trẻ nhất

--- demo/N0905B-YoungerMan.cpp
+++ demo/N0905B-YoungerMan.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 using namespace std;
-struct CAUTHU{
-    string name;
-	int weight,height,age;
+struct CAUTHU {
+	string name;
+	int weight, height, age;
 };
 int main() {
-	int n;
-	cin>>n;
+	int n, k = 0;
+	cin >> n;
 	CAUTHU a[n];
-	for(int i=0;i<n;++i) cin>>a[i].name>>a[i].weight>>a[i].height>>a[i].age;
+	for (int i = 0; i < n; ++i) cin >> a[i].name >> a[i].weight >> a[i].height >> a[i].age;
 	int min = a[0].age;
-	for(int i=0;i<n;++i){
-		if(a[i].age < min) cout<<a[i].name<<" "<<a[i].weight<<" "<<a[i].height<<" "<<a[i].age;
+	for (int i = 0; i < n; ++i) {
+		if (a[i].age < min) {
+			min = a[i].age;
+			k = i;
+		}
 	}
+	cout << a[k].name << " " << a[k].weight << " " << a[k].height << " " << a[k].age;
 }
