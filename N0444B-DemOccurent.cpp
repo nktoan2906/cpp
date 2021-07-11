f33cb750306d87ae59e9440bcdbaeec268dc2d3c
hyn <mrsay132@gmail.com>
2021-07-11 21:09:28 +0700

    đếm số lần xuất hiện của từng pt trong mảng

---
+++ N0444B-DemOccurent.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#define FOR(i,n) for (ulli i = 1; i <= n; ++i)
+#define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ulli unsigned long long int
+using namespace std;
+const int N = 1e6 + 7;
+ulli a[N],n, dem[N], count[N];
+int main() {
+    FASTER;    
+    cin >> n;
+    FOR(i,n) {
+        cin >> a[i];
+        ++dem[a[i]];
+    }
+    FOR(i,n) { 
+        ++count[a[i]];
+        if (count[a[i]] == 1) cout << a[i] << ' ' << dem[a[i]] << '\n';
+    }
+}
