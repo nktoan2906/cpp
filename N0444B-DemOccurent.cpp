9b6e2faa9b8fb701a0da4735e023a90a47088cb3
hyn <mrsay132@gmail.com>
2021-07-11 21:34:18 +0700

    đếm occurent của a[i] ( sort nếu có )

--- N0444B-DemOccurent.cpp
+++ N0444B-DemOccurent.cpp
@@ -4,7 +4,7 @@
 #define ulli unsigned long long int
 using namespace std;
 const int N = 1e6 + 7;
-ulli a[N],n, dem[N], count[N];
+ulli a[N],n, dem[N], anotherdem[N];
 int main() {
     FASTER;    
     cin >> n;
@@ -12,8 +12,9 @@ int main() {
         cin >> a[i];
         ++dem[a[i]];
     }
+    // sort(a + 1, a + n + 1);
     FOR(i,n) { 
-        ++count[a[i]];
-        if (count[a[i]] == 1) cout << a[i] << ' ' << dem[a[i]] << '\n';
+        ++anotherdem[a[i]];
+        if (anotherdem[a[i]] == 1) cout << a[i] << ' ' << dem[a[i]] << '\n';
     }
 }
