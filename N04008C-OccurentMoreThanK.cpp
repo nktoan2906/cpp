86dd8e752978fdbb8bce1992b54ecbd86f33b176
hyn <mrsay132@gmail.com>
2021-07-19 13:09:14 +0700

    in ra occurent >= k

--- N04008C-OccurentMoreThanK.cpp
+++ N04008C-OccurentMoreThanK.cpp
@@ -2,18 +2,20 @@
 #include <map>
 #define lli unsigned long long int
 #define FASTER ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+#define RUN for (lli i = 1; i <= n; i++)
+#define RUNAUTO for (auto it : dem)
 using namespace std;
 lli n, k, a[1000001];
 map<lli, lli> dem;
 int main() {
 	FASTER;
 	cin >> n >> k;
-	for (lli i = 1; i <= n; i++) {
+	RUN {
 		cin >> a[i];
 		dem[a[i]]++;
 	}
 	lli t = 1;		
-	for (auto it : dem) {
+	RUNAUTO {
 		if (it.second >= k){
 			t = 0;
 			cout << it.first << ' ';
