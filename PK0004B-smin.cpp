3a22945f6b4a7045e33ba10b2e5bdd8d4bbd781e
hyn <mrsay132@gmail.com>
2021-07-19 13:06:47 +0700

    in ra smin

--- PK0004B-smin.cpp
+++ PK0004B-smin.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <algorithm>
+#define NEWRUN for (int i = 1; i <= n; cin >> a[i++]);
+#define RUN2 for (int i = 2; i <= n; i++)
 using namespace std;
 int result = 1000000005;
 int main()
@@ -7,9 +9,9 @@ int main()
     int n;
     cin >> n;
     int a[n];
-    for (int i = 1; i <= n; cin >> a[i++]);
+    NEWRUN;
     sort(a + 1, a + n + 1);
-    for (int i = 2; i <= n; i++){
+    RUN2{
         result = min(result, a[i] - a[i - 1]);
     }
     cout << result;
