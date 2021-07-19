2764367206676d5a22d52a0f215d364d02163a29
hyn <mrsay132@gmail.com>
2021-07-20 06:55:44 +0700

    đếm num A Giao B

--- N0614C-AGiaoB.cpp
+++ N0614C-AGiaoB.cpp
@@ -2,16 +2,19 @@
 #include <map>
 using namespace std;
 #define FASTER ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-#define FOR(i, l, r) for (int i = (l); i <= (r); ++i)
+#define RUNS for (int i = 0; i < s.size(); ++i)
+#define RUNS1 for (int i = 0; i < s1.size(); ++i)
+#define RUN for (int i = 1; i <= 255; ++i)
+typedef map<char, int> mci;
 string s, s1;
 int main() {
 	FASTER;
 	cin >> s >> s1;
-	map<char, int> dem, dem1;
+	mci dem, dem1;
 	int count = 0;
-	for (int i = 0; i < s.size(); ++i) ++dem[s[i]];
-	for (int i = 0; i < s1.size(); ++i) ++dem1[s1[i]];
-	for (int i = 1; i <= 255; ++i) { //65 -> 125
+	RUNS ++dem[s[i]];
+	RUNS1 ++dem1[s1[i]];
+	RUN { //65 -> 125
 		if (dem[i] > 0 && dem1[i] > 0) ++count; // count = 1; // cout << char(i);
 	}
 	cout<<count; // if (t = 0) cout << 0;
