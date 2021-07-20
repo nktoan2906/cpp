6d1bef559d662d57ba0ba2a6a52421b00dd4e768
hyn <mrsay132@gmail.com>
2021-07-20 11:18:47 +0700

    tính sum mảng a từ 1 đến vị trí p

---
+++ IDP1104A-SumAiP.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#define RUN for(int i = 1; i <= n; ++i)
+#define RUNQ for(int i = 1; i <= q; ++i)
+#define ulli unsigned long long int
+using namespace std;
+int main() {
+    int n, q, p;
+    cin >> n;
+    vector < int > a(n + 1), s(n + 1);
+    s[0] = 0;
+    RUN {
+    	cin >> a[i];
+    	s[i] = s[i - 1] + a[i];
+    }
+    cin >> q;
+    RUNQ {
+        cin >> p;
+        cout << s[p] << '\n';
+    }
+}
