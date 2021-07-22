cf945a9ccc1cdcc8bf3c9f0b4693f38d6b9bd6b6
hyn <mrsay132@gmail.com>
2021-07-22 22:46:47 +0700

    tính tổ hợp

--- IMA2004A-tohop.cpp
+++ IMA2004A-tohop.cpp
@@ -1,14 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int C(int k, int n) {
     if (k == 0 || k == n) return 1;
-    if (k == 1) return n;
+    if (k == 1){
+        return n;
+    }
     return C(k - 1, n - 1) + C(k, n - 1);
 }
-int main(){
+int main() {
     int n, k;
-    cin>>k>>n;
+    cin >> k >> n;
     cout << C(k, n);
     return 0;
 }
-    
