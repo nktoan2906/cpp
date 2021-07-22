67b08a058e9c479c1c097c35823ee27ab1209178
hyn <mrsay132@gmail.com>
2021-07-22 22:52:28 +0700

    tính tổ họp % base

--- IMA2004B-ToHopModBase.cpp
+++ IMA2004B-ToHopModBase.cpp
@@ -1,26 +1,24 @@
-#include<iostream>
-#define FOR(i,k) for (int i = 0; i <= k; ++i)
-#define FOR1(j,n) for (int j = i; j <= n; ++j)
+#include <iostream>
+#define RUNK for (int i = 0; i <= k; ++i)
+#define RUNJIN for (int j = i; j <= n; ++j)
 using namespace std;
 #define ll long long
-#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-const ll mod = 1e9 + 7;
-ll n,k,m;
-ll a[5005][5005];
-ll C(ll k, ll n,ll m) {
-    if (k == 0||k == n) return 1;
-    else{
-        FOR(i,k){
-            FOR1(j,n){
-                if (i == 0||i == j) a[i][j] = 1;
+#define FASTER ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+ll n, k, m, a[5005][5005];
+ll C(ll k, ll n, ll m) {
+    if (k == 0 || k == n) return 1;
+    else {
+        RUNK {
+            RUNJIN {
+                if (i == 0 || i == j) a[i][j] = 1;
                 else a[i][j] = a[i][j - 1] % m + a[i - 1][j - 1] % m;
-			}
-		}
-	}
+            }
+        }
+    }
     return a[k][n];
 }
 signed main() {
-    fast;
+    FASTER;
     cin >> k >> n >> m;
-    cout<<C(k, n , m) % m;
+    cout << C(k, n, m) % m;
 }
