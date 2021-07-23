23c610a13626c8bc67c144c1c10c58e0fbb5c2d5
hyn <mrsay132@gmail.com>
2021-07-23 08:23:58 +0700

    check số vui vẻ trong đoạn [a,b]

--- N0811B-SoVuiVe.cpp
+++ N0811B-SoVuiVe.cpp
@@ -2,17 +2,17 @@
 #define ll long long
 #define ulli unsigned long long int
 #define happynumchan happynum % 2 == 0
-#define FOR(a,b) for (ulli i = a; i <= b; ++i) 
-ulli a, b, dem, happynum, chusocuan;
+#define RUNAB for (ulli i = a; i <= b; ++i) 
+ulli a, b, dem, happynum, n10;
 using namespace std;
 int main() {
     cin >> a >> b;
-    FOR(a,b){
-        chusocuan = i;
+    RUNAB{
+        n10 = i;
         happynum = 0;
-        while (chusocuan != 0) {
-            happynum += ((chusocuan % 10) * (chusocuan % 10));
-            chusocuan /= 10;
+        while (n10 != 0) {
+            happynum += ((n10 % 10) * (n10 % 10));
+            n10 /= 10;
         }
         if (happynumchan) ++dem;
     }
