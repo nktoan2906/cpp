6b73e09821076fcd4b863be11fbd0c11c86d3e55
hyn <mrsay132@gmail.com>
2021-07-23 07:46:48 +0700

    check hợp số đặc biệt

--- PK0007C-SpecialHopSo.cpp
+++ PK0007C-SpecialHopSo.cpp
@@ -1,22 +1,21 @@
-#pragma GCC optimize(2) /* code chạy nhan hơn*/ 
-#pragma GCC optimize(3) /* code chạy nhan hơn*/ 
 #include<iostream>
 #include <vector>
 using namespace std;
 #define FASTER ios_base::sync_with_stdio(0);cin.tie(0)  /* code chạy nhan hơn*/ 
 #define ll long long
+#define nmodi n % i == 0
 #define l(x) (ll)x.size()
 typedef vector<ll> vll;
 bool check(ll n) {
     vll a;
     ll i = 2;
     while(i <= n / i) { // dùng while() để ... 
-        if(n % i == 0) {  // ... tìm SNT chia hết cho i 
+        if(nmodi) {  // ... tìm SNT chia hết cho i 
             a.push_back(i); // đếm xem có bao nhiêu SNT nhân với nhau
             n /= i;
             if(l(a) > 2) return false; // nếu trên 2 -> F 
         }
-        else i++;
+        else ++i;
     }
     a.push_back(n);
     if(l(a) == 2){
@@ -26,9 +25,8 @@ bool check(ll n) {
 }
 signed main() {
     FASTER;
-    ll q; cin >> q;
+    ll q, x; cin >> q;
     while(q--){ // while(q--) giống while(q > 0) nhưng mà q-- tốt hơn 
-        ll x; 
 		cin >> x;
         if(check(x)) cout << "Yes" << '\n';
         else cout << "No" << '\n';
