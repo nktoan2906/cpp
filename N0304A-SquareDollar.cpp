358f62b5d978fa66006f7977b63cf7e998c771d9
hyn <mrsay132@gmail.com>
2021-07-23 07:36:33 +0700

    in ra hình vuông dấu $

--- N0304A-SquareDollar.cpp
+++ N0304A-SquareDollar.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#define FOR(i,n) for (int i = 1; i <= n; ++i) 
-#define FOR(j,n) for (int j = 1; j <= n; ++j) 
+#define RUN for (int i = 1; i <= n; ++i)
+#define RUNJ for (int j = 1; j <= n; ++j)
 using namespace std;
 int main() {
     int n;
     cin >> n;
-    FOR(i,n){
-        FOR(j,n) {
+    RUN {
+        RUNJ {
             cout << "$"; //luu y,co mot khoang trong o day
         }
         cout << "\n";
