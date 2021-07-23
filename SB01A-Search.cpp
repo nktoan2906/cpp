95212038c0f69881787fc732cc89164e0654c207
hyn <mrsay132@gmail.com>
2021-07-23 07:38:30 +0700

    in ra Y nếu có x trong mảng, else N

--- SB01A-Search.cpp
+++ SB01A-Search.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#define FOR(i,n) for (ll i = 1; i <= n; i++)
+#define RUN for (ll i = 1; i <= n; i++)
 using namespace std;
 #define ll long long
 int main() {
 	ll n, x;
 	cin >> n >> x;
 	ll a[n];
-	FOR(i,n) cin >> a[i];
-	FOR(i,n) {
+	RUN cin >> a[i];
+	RUN {
 		if (a[i] == x) {
 			cout << "Y";
 			return 0;
