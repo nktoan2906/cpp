113a5f17e494b68b5afa81eb94df02612993edf6
hyn <mrsay132@gmail.com>
2021-07-23 07:47:49 +0700

    chuyển n đĩa 1->3 (chuyển đc 1 đĩa, nhở đè lớn)

--- N1115E-HNTower.cpp
+++ N1115E-HNTower.cpp
@@ -1,16 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void THN(int n, int a, int b, int c){
-	if(n==1){
-		cout<<a<<" "<<c<<endl;
-		return ;
+void HNTower(int n, int a, int b, int c) {
+	if (n == 1) {
+		cout << a << " " << c << endl;
+		return;
 	}
-	THN(n-1,a,c,b);
-	THN(1,a,b,c);
-	THN(n-1,b,a,c);
+	HNTower(n - 1, a, c, b);
+	HNTower(1, a, b, c);
+	HNTower(n - 1, b, a, c);
 }
-int main(){
-    int n,a=1,b=2,c=3;
-	cin>>n;
-	THN(n,1,2,3);
+int main() {
+	int n, a = 1, b = 2, c = 3;
+	cin >> n;
+	HNTower(n, 1, 2, 3);
 }
