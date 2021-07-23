8402d1a271660aeefb7cdfd650ff4bb5d9c75fb3
hyn <mrsay132@gmail.com>
2021-07-23 07:39:53 +0700

    in ra số ngày để gia công n sp

--- PK0021B-giacongsp.cpp
+++ PK0021B-giacongsp.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #define ll long long
 using namespace std;
-ll spsum,sp,songay;
+ll spsum, sp, kq;
 int main() {
-    cin>>spsum>>sp;
-    while(spsum>0) {
-        spsum-=sp;
-        sp*=2;
-        songay++;
+    cin >> spsum >> sp;
+    while (spsum > 0) {
+        spsum -= sp;
+        sp *= 2;
+        ++kq;
     }
-    cout<<songay;
+    cout << kq;
     return 0;
 }
