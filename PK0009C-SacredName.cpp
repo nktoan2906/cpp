cf988e9da6a2c71eb9162d1caeeb65b5aa35a732
hyn <mrsay132@gmail.com>
2021-07-23 07:41:43 +0700

    in ra số các xâu con mà ko trùng nhau

--- PK0009C-SacredName.cpp
+++ PK0009C-SacredName.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 #include <map>
 #define FASTER ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-using namespace std;
 #define ll long long
+#define RUNSTRING for (int i = 0; i < s.length(); ++i)
+#define RUNJSTRING for (int j = i; j < s.length(); ++j)
+#define RUN for (int i = 1; i <= dem; ++i)
+using namespace std;
 string s, a[1000005];
 map <string, ll> b;
 ll dem = 0, res = 0;
 int main() {
 	FASTER;
 	cin >> s;
-	for (int i = 0; i < s.length(); ++i) {
-		for (int j = i; j < s.length(); ++j) {
+	RUNSTRING {
+		RUNJSTRING {
 			++dem;
 			a[dem] = s.substr(i, j - i + 1);
 		}
 	}
-	for (int i = 1; i <= dem; ++i) {
+	RUN {
 		if (a[i] != " ") ++b[a[i]];
 	}
-	for (int i = 1; i <= dem; ++i) {
+	RUN {
 		if (b[a[i]] > 0) {
 			++res;
 			b[a[i]] = 0;
