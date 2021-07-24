a677908a79fb4431466c9d2118628d0313896571
hyn <mrsay132@gmail.com>
2021-07-24 07:46:33 +0700

    in ra tam giác ngược số 0

--- N0305A-TamGiac0Xuoi.cpp
+++ N0305A-TamGiac0Xuoi.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
-#define FOR(i,n) for(int i = 1; i <= n; ++i)
-#define FOR1(j,i) for(int j = 1; j <= i; ++j)
-#define ll long long 
+#define RUN for (int i = 1; i <= n; ++i)
+#define RUNJI for (int j = 1; j <= i; ++j)
+#define ll long long
 using namespace std;
 ll n;
 int main() {
     cin >> n;
-    FOR(i,n) {
-        FOR1(j,i){
+    RUN {
+        RUNJI {
             cout << "0";
         }
         cout << endl;
     }
     return 0;
 }
-    
