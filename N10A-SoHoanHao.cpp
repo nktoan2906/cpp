f61408d0e310a3b6ee8105b46f5dbc9ab1bcd344
hyn <mrsay132@gmail.com>
2021-07-28 17:15:15 +0700

    check số hoàn hảo

--- N10A-SoHoanHao.cpp
+++ N10A-SoHoanHao.cpp
@@ -7,9 +7,9 @@ int n, s = 0;
 int main() {
     cin >> n;
     RUNSQRT {
-        if (nmodi) s += i + n / i;
+        if (nmodi) s += i + n / i; // tính sum ước của n ( trừ chính nó ) -> n phải chia đôi 
     }
-    if (s == 2 * n) cout << "YES";
+    if (s == 2 * n) cout << "YES"; // n chia đôi -> tổng gấp đôi -> check s có = 2n hay ko  
     else cout << "NO";
     return 0;
 }
