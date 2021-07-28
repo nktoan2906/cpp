dc6fcf16a9ee6b7f16e7315c6f6acb0e737e0e24
hyn <mrsay132@gmail.com>
2021-07-28 16:55:43 +0700

    tính C % base

--- IMA2004B-ToHopModBase.cpp
+++ IMA2004B-ToHopModBase.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#define RUNK for (int i = 0; i <= k; ++i)
-#define RUNJIN for (int j = i; j <= n; ++j)
+#define RUNK for (int K = 0; K <= k; ++K)
+#define RUNN for (int N = K; N <= n; ++N)
 using namespace std;
 #define ll long long
 #define FASTER ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
@@ -9,9 +9,9 @@ ll C(ll k, ll n, ll m) {
     if (k == 0 || k == n) return 1;
     else {
         RUNK {
-            RUNJIN {
-                if (i == 0 || i == j) a[i][j] = 1;
-                else a[i][j] = a[i][j - 1] % m + a[i - 1][j - 1] % m;
+            RUNN {
+                if (K == 0 || K == N) a[K][N] = 1;
+                else a[K][N] = a[K][N - 1] % m + a[K - 1][N - 1] % m;
             }
         }
     }
