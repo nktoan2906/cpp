7c174e683708d25350f8e4e4e37c114a299c3791
hyn <mrsay132@gmail.com>
2021-07-28 16:51:45 +0700

    tính tổ hợp

--- IMA2004A-tohop.cpp
+++ IMA2004A-tohop.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 int C(int k, int n) {
-    if (k == 0 || k == n) return 1;
-    if (k == 1){
+    if (k == 0 || k == n) return 1; // C(0,n) = C(n,n) = 1
+    if (k == 1){ // C(1,n) = n! / (n - 1)! = n! : (n! / n)  = n!n / n! = n 
         return n;
     }
-    return C(k - 1, n - 1) + C(k, n - 1);
+    return C(k - 1, n - 1) + C(k, n - 1); // C(k,n) = C(k - 1, n - 1) + C(k, n - 1)
 }
 int main() {
     int n, k;
