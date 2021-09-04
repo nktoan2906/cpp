aa181df9921f13e47e4f646ce0ad0f93853a9076
hyn <mrsay132@gmail.com>
2021-09-04 20:05:06 +0700

    in ra lmax

--- 2107E-IncreaseLienTiepA.cpp
+++ 2107E-IncreaseLienTiepA.cpp
@@ -1,24 +1,23 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#define faster ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
-#define el cout << "\n"
-#define f0(i, n) for (int i = 0; i < n; i++)
-#define f1(i, n) for (int i = 1; i <= n; i++)
+#define RUN for (int i = 1; i <= n; i++)
 #define maxn 100005
-int n, a[maxn], b[maxn], f[2][maxn], ans;
+int n, a[maxn], f[2][maxn], max_length;
 int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    faster;
     cin >> n;
-    f1(i, n) cin >> a[i];
+    RUN cin >> a[i];
     a[0] = -1e9;
-    f1(i, n) {
+    RUN {
         if (a[i] > a[i - 1]) f[0][i] = f[0][i - 1] + 1, f[1][i] = f[1][i - 1] + 1;
         else f[0][i] = 1, f[1][i] = 1;
         if (i > 1) {
             if (a[i] > a[i - 2] + 1) f[1][i] = max(f[1][i], f[0][i - 2] + 2);
         }
-        ans = max(ans, f[1][i]);
-        ans = max(ans, min(f[0][i] + 1, n));
+        max_length = max(max_length, f[1][i]);
+        max_length = max(max_length, min(f[0][i] + 1, n));
     }
-    cout << ans;
+    cout << max_length;
     return 0;
 }
