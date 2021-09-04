4e4d6980784821a3f7dd4ee0b9382d57ad3bc916
hyn <mrsay132@gmail.com>
2021-09-04 20:34:14 +0700

    biến đổi | num số >= một value cho trước là max

--- 2107F-ChangeNum.cpp
+++ 2107F-ChangeNum.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <algorithm>
-using namespace std;
 #define RUN for (int i = 1; i <= n; i++)
 #define MAX 100005
+#define faster ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
 long long n, truyvan, a[MAX], vt_greater_than_k, s[MAX];
-bool check(int delete_num, int value) {
+bool SumDon(int delete_num, int value) { 
     return (delete_num + s[vt_greater_than_k - 1] - s[delete_num] >= 1ll * (vt_greater_than_k - delete_num - 1) * value);
 }
 int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    faster;
     cin >> n >> truyvan;
     RUN cin >> a[i];
     sort(a + 1, a + n + 1);
-    RUN s[i] = s[i - 1] + a[i];
+    RUN s[i] = s[i - 1] + a[i]; // ct truy vấn 
     while (truyvan--) {
         int value;
         cin >> value;
@@ -24,7 +25,7 @@ int main() {
         int left = 0, right = vt_greater_than_k;               // C H Ặ T  
         while (right - left > 1) {               //   
             int mid = (left + right) / 2;        // N H Ị  
-            if (check(mid, value)) right = mid;  //    
+            if (SumDon(mid, value)) right = mid;  //    
             else left = mid;                     // P H Â N
         }                                        //    
         cout << n - right << "\n";
