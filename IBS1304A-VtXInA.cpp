86ccfe2ecc287df9fcff7db0f90b8b495294dbd2
hyn <mrsay132@gmail.com>
2021-09-10 23:24:15 +0700

    in ra vtx trong mảng a

--- IBS1304A-VtXInA.cpp
+++ IBS1304A-VtXInA.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
+#include <map>
 using namespace std;
 int main() {
-    long long int n, truyvan, x; 
+    long long n, ntruyvan, truyvan; 
     cin >> n;
     long long int a[n + 1];
-    for (int i = 1; i <= n; ++i) cin >> a[i];
-    cin >> truyvan;
-    for (int j = 1; j <= truyvan; ++j) {
-        cin >> x;
-        int left = 1, right = n, m, vt = 0;
-        while (left <= right) {
-            m = (left + right) / 2;
-            if (a[m] == x) {
-                vt = m;
-                break;
-            }
-            if (a[m] < x) left = m + 1;
-            else right = m - 1;
-        }
-        cout << vt << endl;
+    map<long long, long long> vt;
+    for (int i = 1; i <= n; ++i) {
+        cin >> a[i];
+        vt[a[i]] = i;
+    }
+    cin >> ntruyvan;
+    for (int j = 1; j <= ntruyvan; ++j) {
+        cin >> truyvan;
+        cout << vt[truyvan] << '\n';
     }
 }
+    
