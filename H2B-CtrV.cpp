79c98b5061896c3bb4a313574c4a7312422977d2
hyn <mrsay132@gmail.com>
2021-11-05 08:46:28 +0700

    chèn thêm pt vào mảng

--- H2B-CtrV.cpp
+++ H2B-CtrV.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #define lli long long int
+#define RUN(i, a, b) for (lli i = a; i <= b; ++i)
 using namespace std;
 int main() {
     lli a[1000001], n, x, k;
     cin >> n >> k >> x;
-    for (lli i = 1; i <= n; i++) cin >> a[i]; 
-    for(lli i = 1; i <= n; i++) {
+    RUN(i, 1, n) cin >> a[i]; 
+    RUN(i, 1, n) {
         if(i == k) cout << x << " " << a[i] << " ";
-        else cout << a[i] << " ";
-
+        else cout << a[i] << " "; 
     }
 }
