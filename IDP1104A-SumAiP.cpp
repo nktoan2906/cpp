aa4ef964565d7936988f6d9748df359703b243fd
hyn <mrsay132@gmail.com>
2021-11-05 08:49:34 +0700

    tính sum mảng a từ 1 đến pos p (l -> r)

--- IDP1104A-SumAiP.cpp
+++ IDP1104A-SumAiP.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 #include <vector>
-#define RUN for(int i = 1; i <= n; ++i)
-#define RUNQ for(int i = 1; i <= q; ++i)
+#define RUN(i, a, b) for (ulli i = a; i <= b; ++i)
 #define ulli unsigned long long int
 using namespace std;
+typedef vector < ulli > vi; 
 int main() {
-    int n, q, p;
+    ulli n, q, p;
     cin >> n;
-    vector < int > a(n + 1), s(n + 1);
+    vi a(n + 1), s(n + 1);
     s[0] = 0;
-    RUN {
+    RUN(i, 1, n) {
     	cin >> a[i];
     	s[i] = s[i - 1] + a[i];
     }
     cin >> q;
-    RUNQ {
-        cin >> p;
+    RUN(i, 1, q) {
+        cin >> p; // cin >> l >> r;
         cout << s[p] << '\n'; // s(r) - s(l - 1); 
     }
 }
