52c4be90b6035130b6edbc17223e8042a98452c3
hyn <mrsay132@gmail.com>
2022-02-23 14:09:19 +0700

    tìm số nguyên thứ k trong dãy đã sort

--- BNHAN.Multiply.cpp
+++ BNHAN.Multiply.cpp
@@ -1,18 +1,16 @@
 #include <bits/stdc++.h>
+#define RUN(i, a, b) for (int i = a; i <= b; ++i) 
 using namespace std;
-long long n, m, k, a[1000005],inhanj= 0;
-int main()
-{
-    cin >> n >> m >> k;
-    for(int i = 1; i <= n; i++)
-    {
-        for(int j = 1; j <= m; j++)
-        {
-            inhanj++;
+long long dong, cot, integer, a[1000005], inhanj= 0;
+int main() {
+    cin >> dong >> cot >> integer;
+    RUN(i, 1, dong) {
+        RUN(i, 1, cot) {
+            ++inhanj; 
             a[inhanj] = i * j;
         }
     }
-    sort(a+1,a+inhanj+1);
-    cout << a[k];
+    sort(a+1,a+inhanj+1); 
+    cout << a[integer];
 }
     
