3e73ca75501cc32fa2bd653367684f7a88920512
hyn <mrsay132@gmail.com>
2022-02-23 13:56:17 +0700

    team trường sở tại sẽ có bao nhiêu điểm

---
+++ GIDA-Fight.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define RUN0(i, a, b) for (int i = a; i < b; ++i) 
+using namespace std;
+int main() {
+    long long int n, res = 0, score = 0;
+    cin >> n;
+    long long int dang_cai_school[n + 1], so_tai_school[n + 1];
+    RUN0(i, 0, n) cin >> dang_cai_school[i];
+    RUN0(i, 0, n) cin >> so_tai_school[i];
+    sort(dang_cai_school, dang_cai_school + n);
+    sort(so_tai_school, so_tai_school + n);
+    RUN0(i, 0, n) {
+        if(dang_cai_school[score] < so_tai_school[i]) {
+            ++score;
+            ++res;
+        }
+    }
+    cout << res;
+}
+    
