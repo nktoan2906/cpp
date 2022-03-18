d46fe5e1f593127d06e1b1f4d3c8d1e524da8052
hyn <mrsay132@gmail.com>
2022-03-18 07:13:48 +0700

    ghép tối đa đc bao nhiêu cặp

---
+++ KIVU2-Dance2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define ll long long
+#define RUN(i, begin, end) for (ll i = begin; i <= end; ++i)
+using namespace std;
+typedef vector<ll> vi;
+const ll N = 1e6 + 17;
+ll n, cnt;
+vi boy, boy1, girl, girl1;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	cin >> n;
+	RUN(i, 1, n) {
+		ll x;
+		cin >> x;
+		if (x > 0) boy.push_back(x);
+		else boy1.push_back(abs(x));
+	}
+	RUN(i, 1, n) {
+		ll x;
+		cin >> x;
+		if (x > 0) girl.push_back(x);
+		else girl1.push_back(abs(x));
+	}
+	sort(boy.begin(), boy.end());
+	sort(boy1.begin(), boy1.end());
+	sort(girl.begin(), girl.end());
+	sort(girl1.begin(), girl1.end());
+	ll i = 0, j = 0;
+	while (i < boy.size() && j < girl1.size()) {
+		if (boy[i] < girl1[j]) {
+		    ++cnt;
+			++i;
+			++j;
+		}
+		else ++j;
+	}
+	i = 0, j = 0;
+	while (i < boy1.size() && j < girl.size()) {
+		if (boy1[i] > girl[j]) {
+		    ++cnt;
+			++i;
+			++j;
+		}
+		else ++i;
+	}
+	cout << cnt;
+}
