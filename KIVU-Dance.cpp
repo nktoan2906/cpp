ec34cee9a7a9673683b4400e6fa35997bf790275
hyn <mrsay132@gmail.com>
2022-03-23 06:44:06 +0700

    tính số lượng cặp mà nam cao hơn nữ, ko quá 1 cặp

--- KIVU-Dance.cpp
+++ KIVU-Dance.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 #define RUN(i, a, b) for (int i = a; i <= b; ++i)
 using namespace std;
-long long boys, girls, boy_height[100005], girl_height[100005];
+long long boys, girls, boy_h[100005], girl_h[100005];
 int main() {
     cin >> boys >> girls;
-    RUN(i, 1, boys) cin >> boy_height[i];
-    RUN(i, 1, girls) cin >> girl_height[i];
-    sort(boy_height + 1, boy_height + boys + 1);
-    sort(girl_height + 1, girl_height + girls + 1);
+    RUN(i, 1, boys) cin >> boy_h[i];
+    RUN(i, 1, girls) cin >> girl_h[i];
+    sort(boy_h + 1, boy_h + boys + 1);
+    sort(girl_h + 1, girl_h + girls + 1);
     int pairnum = 0, j = 1;
     RUN(i, 1, boys) {
-        if (boy_height[i] > girl_height[j]) {
-            pairnum ++;
-            j++;
+        if (boy_h[i] > girl_h[j]) {
+            ++pairnum;
+            ++j;
         }
 	}
     cout << pairnum;
