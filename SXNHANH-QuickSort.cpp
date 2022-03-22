060cf5de5953cbbdd1343cb37d0d5fc34b6a9561
hyn <mrsay132@gmail.com>
2022-03-23 06:55:29 +0700

    sắp xếp nhanh

--- SXNHANH-QuickSort.cpp
+++ SXNHANH-QuickSort.cpp
@@ -1,22 +1,22 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #define ll long long
 #define RUN(i, a, b) for (int i = a; i <= b; ++i)
 using namespace std; 
 const ll N = 1e6;
-typedef pair<ll,ll> ii;
 int n, a[N], dem = 1;
+
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
     cin >> n;
-    for (int i = 1; i <= n; i++)  cin >> a[i];
-    for (int i = 1; i <= n; i++) {
+    RUN(i, 1, n)  cin >> a[i];
+    RUN(i, 1, n) {
         if (a[i] <= a[n]) {
             swap(a[i], a[dem]);
             ++dem;
         }
     }
     swap(a[dem], a[n]);
-    for (int i = 1; i <= n; i++) {
+    RUN(i, 1, n) {
         if (dem == i) cout << '[' << a[i] << "] "; 
         else cout << a[i] << " ";
     }
