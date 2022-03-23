d12c6e200744a3ed5bce75ed83484521e8e138bd
hyn <mrsay132@gmail.com>
2022-03-23 07:29:54 +0700

    Tính tổng S = 1^2 + 2^2 + ... + n^2

--- N0310B-tinhsum1.cpp
+++ N0310B-tinhsum1.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #define ll long long
 using namespace std;
-int main(){
+
+int main() {
     ll n;
-    cin>>n;
-    ll s = (n * ( n + 1 ) * ( 2 * n + 1 ))/6;
-    cout<<s;
+    cin >> n;
+    ll s = (n * ( n + 1 ) * ( 2 * n + 1 )) / 6;
+    cout << s;
     return 0;
 }
