ffb6da9ea8e59d8c6c2c49e27a81277b031f7916
hyn <mrsay132@gmail.com>
2022-03-23 07:24:54 +0700

    In số hạng thứ n của dãy

--- N0309C-dayso.cpp
+++ N0309C-dayso.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
+#include <math.h>
 #define ll long long
-#define FOR3(i,n) for( ll i=3; i<=n; ++i)
+#define RUN(i, begin, end) for (ll i = begin; i <= end; ++i)
 using namespace std;
 const int mod = 1e9 + 7;
-ll a[1000002],n;
+ll a[1000002], n;
+
 int main() {
-    cin>>n;
-    a[1] = a[2] = 1;
-    FOR3(i,n) {
-        a[i] = ((3*a[i-1] % mod ) - (a[i-2] % mod)) % mod;
-        if (a[i] < 0) a[i] = mod - a[i] * -1;
-    }
-    cout<<a[n];
+    cin >> n;
+    // a[1] = a[2] = 1;
+    // RUN(i, 3, n) {
+    //     a[i] = ((3 * a[i - 1] % mod) - (a[i - 2] % mod)) % mod;
+    //     if (a[i] < 0) a[i] = mod - a[i] * -1;
+    // }
+    // cout << a[n];
+    cout << pow(n,2);
     return 0;
 }
-    
