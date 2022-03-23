7b98f4e8d9566da0795e331ee9516365f5a46d84
hyn <mrsay132@gmail.com>
2022-03-23 07:31:16 +0700

    in ra s(n) = 1! = 2! + ... + n! % (1e9 + 7)

--- N0310C-sumfactor.cpp
+++ N0310C-sumfactor.cpp
@@ -1,15 +1,14 @@
-//http://laptrinhphothong.vn/Problem/Details/4580
 #include <iostream>
+#define ll long long
 using namespace std;
-unsigned long long n,s,f;
+#define RUN(i, begin, end) for (ll i = begin; i <= end; i++)
 int main() {
+    ll n, s = 0, f = 1;
 	cin >> n;
-    f = 1;
-	s = 0;
-	long long mod = 1e9 + 7;
-	for(long long i = 1;i<=n;i++){
-		f = (f*i) % mod;
-		s = (s+f) % mod;
+	ll mod = 1e9 + 7;
+	RUN(i, 1, n) {
+		f = (f * i) % mod;
+		s = (s + f) % mod;
 	}
-	cout<<s;
+	cout << s;
 }
