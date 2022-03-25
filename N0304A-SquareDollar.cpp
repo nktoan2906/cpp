a4218c1fdc5ad8a5c6231f6e28308b8daad74db0
hyn <mrsay132@gmail.com>
2022-03-25 07:20:39 +0700

    In ra một hình vuông n * n gồm các dấu $

--- N0304A-SquareDollar.cpp
+++ N0304A-SquareDollar.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#define RUN for (int i = 1; i <= n; ++i)
-#define RUNJ for (int j = 1; j <= n; ++j)
+#define RUN(i, begin, end) for (int i = begin; i <= end; ++i)
 using namespace std;
+
 int main() {
     int n;
     cin >> n;
-    RUN {
-        RUNJ {
-            cout << "$"; //luu y,co mot khoang trong o day
+    RUN(i, 1, n) {
+        RUN(j, 1, n) {
+            cout << "$"; //co mot khoang trong o day
         }
         cout << "\n";
     }
