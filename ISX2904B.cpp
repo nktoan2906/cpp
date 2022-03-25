b0982e1c16e2adf9aee1c18d86bb090be07ec13b
hyn <mrsay132@gmail.com>
2022-03-25 08:31:40 +0700

    sort dãy C tm mọi cặp i & j mà (1 ≤ i < j ≤ n) thì ai < aj || (ai = aj và bi ≥ bj)

--- ISX2904B.cpp
+++ ISX2904B.cpp
@@ -1,18 +1,22 @@
+//http://laptrinhphothong.vn/ckfinder/userfiles/files/ISX2904B.pdf
 #include <iostream>
 #include <algorithm>
+#define RUN(i, begin, end) for (int i = begin; i <= end; ++i)
 #define ll long long
 using namespace std;
 typedef pair<ll, ll> pll;
 ll n, s, m;
 pll a[1000001];
+
 bool SORT(pll a, pll b) {
 	if(a.first == b.first) return a.second > b.second;
 	return a.first < b.first;
 }
+
 int main() {
 	cin >> n;
-	for (int i = 1; i <= n; ++i) cin >> a[i].first;
-	for (int i = 1; i <= n; ++i) cin >> a[i].second;
+	RUN(i, 1, n) cin >> a[i].first;
+	RUN(i, 1, n) cin >> a[i].second;
     sort(a + 1, a + n + 1, SORT);
-	for (int i = 1; i <= n; ++i) cout << a[i].first << " " << a[i].second << endl;
+	RUN(i, 1, n) cout << a[i].first << " " << a[i].second << endl;
 }
